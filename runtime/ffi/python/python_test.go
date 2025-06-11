87c1e3dc754b612618f6dcf9c432504bf7caf4fa
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-11 08:45:11 +0700

    cleanup python ffi

--- runtime/ffi/python/python_test.go
+++ runtime/ffi/python/python_test.go
@@ -6,30 +6,6 @@ import (
 	"mochi/runtime/ffi/python"
 )
 
-func TestCallStdlib(t *testing.T) {
-	res, err := python.Call("math", "sqrt", 9)
-	if err != nil {
-		t.Fatalf("call math.sqrt: %v", err)
-	}
-	f, ok := res.(float64)
-	if !ok || f != 3 {
-		t.Fatalf("unexpected result: %v", res)
-	}
-}
-
-func TestExecAdd(t *testing.T) {
-	res, err := python.Exec(`
-return args[0] + args[1]
-`, 2, 3)
-	if err != nil {
-		t.Fatalf("exec: %v", err)
-	}
-	n, ok := res.(float64)
-	if !ok || n != 5 {
-		t.Fatalf("unexpected result: %v", res)
-	}
-}
-
 func TestAttrVariable(t *testing.T) {
 	res, err := python.Attr("math", "pi")
 	if err != nil {
