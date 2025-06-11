e180ed5c801a23a8940e732de092fa9aa67b854b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-12 02:17:15 +0700

    Add json tags to infer structs

--- runtime/ffi/infer/infer.go
+++ runtime/ffi/infer/infer.go
@@ -2,65 +2,65 @@ package ffiinfo
 
 // ModuleInfo describes a module's exported symbols and documentation.
 type ModuleInfo struct {
-	Path     string
-	Doc      string
-	Examples []ExampleInfo
+	Path     string        `json:"path,omitempty"`
+	Doc      string        `json:"doc,omitempty"`
+	Examples []ExampleInfo `json:"examples,omitempty"`
 
-	Functions []FuncInfo
-	Vars      []VarInfo
-	Consts    []ConstInfo
-	Types     []TypeInfo
+	Functions []FuncInfo  `json:"functions,omitempty"`
+	Vars      []VarInfo   `json:"vars,omitempty"`
+	Consts    []ConstInfo `json:"consts,omitempty"`
+	Types     []TypeInfo  `json:"types,omitempty"`
 }
 
 // FuncInfo describes an exported function.
 type FuncInfo struct {
-	Name     string
-	Doc      string
-	Params   []ParamInfo
-	Results  []ParamInfo
-	Examples []ExampleInfo
+	Name     string        `json:"name,omitempty"`
+	Doc      string        `json:"doc,omitempty"`
+	Params   []ParamInfo   `json:"params,omitempty"`
+	Results  []ParamInfo   `json:"results,omitempty"`
+	Examples []ExampleInfo `json:"examples,omitempty"`
 }
 
 // VarInfo describes an exported variable.
 type VarInfo struct {
-	Name string
-	Type string
-	Doc  string
+	Name string `json:"name,omitempty"`
+	Type string `json:"type,omitempty"`
+	Doc  string `json:"doc,omitempty"`
 }
 
 // ConstInfo describes an exported constant.
 type ConstInfo struct {
-	Name  string
-	Type  string
-	Value string
-	Doc   string
+	Name  string `json:"name,omitempty"`
+	Type  string `json:"type,omitempty"`
+	Value string `json:"value,omitempty"`
+	Doc   string `json:"doc,omitempty"`
 }
 
 // TypeInfo describes an exported type.
 type TypeInfo struct {
-	Name     string
-	Kind     string
-	Doc      string
-	Fields   []FieldInfo
-	Methods  []FuncInfo
-	Examples []ExampleInfo
+	Name     string        `json:"name,omitempty"`
+	Kind     string        `json:"kind,omitempty"`
+	Doc      string        `json:"doc,omitempty"`
+	Fields   []FieldInfo   `json:"fields,omitempty"`
+	Methods  []FuncInfo    `json:"methods,omitempty"`
+	Examples []ExampleInfo `json:"examples,omitempty"`
 }
 
 type FieldInfo struct {
-	Name string
-	Type string
-	Tag  string
+	Name string `json:"name,omitempty"`
+	Type string `json:"type,omitempty"`
+	Tag  string `json:"tag,omitempty"`
 }
 
 type ParamInfo struct {
-	Name string
-	Type string
+	Name string `json:"name,omitempty"`
+	Type string `json:"type,omitempty"`
 }
 
 type ExampleInfo struct {
-	Name   string
-	Suffix string
-	Doc    string
-	Code   string
-	Output string
+	Name   string `json:"name,omitempty"`
+	Suffix string `json:"suffix,omitempty"`
+	Doc    string `json:"doc,omitempty"`
+	Code   string `json:"code,omitempty"`
+	Output string `json:"output,omitempty"`
 }
