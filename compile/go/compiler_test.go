53fdb1b8caa68a6abde2e70cc46031f2d51fe0f0
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-16 20:49:25 +0700

    test: run leetcode 92 with go compiler

--- compile/go/compiler_test.go
+++ compile/go/compiler_test.go
@@ -109,7 +109,7 @@ func TestGoCompiler_GoldenOutput(t *testing.T) {
 }
 
 func TestGoCompiler_LeetCodeExamples(t *testing.T) {
-	for i := 1; i <= 91; i++ {
+	for i := 1; i <= 92; i++ {
 		runExample(t, i)
 	}
 	runExample(t, 102)
