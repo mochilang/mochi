f29c775676c57b2e72dc4d12ec714bda62428116
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-17 20:16:15 +0700

    swift: support more loops

--- compile/swift/helpers.go
+++ compile/swift/helpers.go
@@ -1,9 +1,13 @@
 package swiftcode
 
 func (c *Compiler) writeln(s string) {
+	c.writeIndent()
+	c.buf.WriteString(s)
+	c.buf.WriteByte('\n')
+}
+
+func (c *Compiler) writeIndent() {
 	for i := 0; i < c.indent; i++ {
 		c.buf.WriteByte('\t')
 	}
-	c.buf.WriteString(s)
-	c.buf.WriteByte('\n')
 }
