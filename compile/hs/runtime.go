1410015d4b8a082cea4e2ccc501c6de4d8c02bd3
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-17 20:30:26 +0700

    Add Haskell compiler backend

---
+++ compile/hs/runtime.go
@@ -0,0 +1,12 @@
+package hscode
+
+const runtime = `
+forLoop :: Int -> Int -> (Int -> Maybe a) -> Maybe a
+forLoop start end f = go start
+  where
+    go i | i < end =
+            case f i of
+              Just v -> Just v
+              Nothing -> go (i + 1)
+         | otherwise = Nothing
+`
