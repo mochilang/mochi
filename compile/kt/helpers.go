3b3b580b8e4bfe5bc7db80c46b441b437b094d18
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-17 18:32:53 +0700

    Revert CLI changes for Kotlin backend

---
+++ compile/kt/helpers.go
@@ -0,0 +1,25 @@
+package ktcode
+
+import "strings"
+
+func sanitizeName(name string) string {
+	if name == "" {
+		return ""
+	}
+	var b strings.Builder
+	for i, r := range name {
+		if r == '_' || ('a' <= r && r <= 'z') || ('A' <= r && r <= 'Z') || ('0' <= r && r <= '9' && i > 0) {
+			b.WriteRune(r)
+		} else {
+			b.WriteRune('_')
+		}
+	}
+	s := b.String()
+	if s == "" {
+		return "_"
+	}
+	if !(s[0] >= 'a' && s[0] <= 'z' || s[0] >= 'A' && s[0] <= 'Z' || s[0] == '_') {
+		s = "_" + s
+	}
+	return s
+}
