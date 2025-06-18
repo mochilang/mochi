e695fc0f0ff5fd383876321af74f68832d0e6c3c
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-19 00:08:37 +0700

    Generate Swift code for LeetCode problems 1-5

--- examples/leetcode-out/swift/2/add-two-numbers.swift
+++ examples/leetcode-out/swift/2/add-two-numbers.swift
@@ -1,36 +1,32 @@
 import Foundation
 
 func addTwoNumbers(_ l1: [Int], _ l2: [Int]) -> [Int] {
-    var i = 0
-    var j = 0
-    var carry = 0
-    var result: [Int] = []
-    while i < l1.count || j < l2.count || carry > 0 {
-        var x = 0
-        if i < l1.count {
-            x = l1[i]
-            i += 1
-        }
-        var y = 0
-        if j < l2.count {
-            y = l2[j]
-            j += 1
-        }
-        let sum = x + y + carry
-        let digit = sum % 10
-        carry = sum / 10
-        result.append(digit)
-    }
-    return result
+	let l1 = l1
+	let l2 = l2
+	
+	var i = 0
+	var j = 0
+	var carry = 0
+	var result: [Int] = []
+	while i < l1.count || j < l2.count || carry > 0 {
+		var x = 0
+		if i < l1.count {
+			x = l1[i]
+			i = i + 1
+		}
+		var y = 0
+		if j < l2.count {
+			y = l2[j]
+			j = j + 1
+		}
+		let sum = x + y + carry
+		let digit = sum % 10
+		carry = sum / 10
+		result = result + [digit]
+	}
+	return result
 }
 
-func example_1() { assert(addTwoNumbers([2,4,3], [5,6,4]) == [7,0,8]) }
-func example_2() { assert(addTwoNumbers([0], [0]) == [0]) }
-func example_3() { assert(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]) == [8,9,9,9,0,0,0,1]) }
-
 func main() {
-    example_1()
-    example_2()
-    example_3()
 }
 main()
--- examples/leetcode-out/swift/5/longest-palindromic-substring.swift
+++ examples/leetcode-out/swift/5/longest-palindromic-substring.swift
@@ -1,75 +1,61 @@
 import Foundation
 
 func _indexString(_ s: String, _ i: Int) -> String {
-    var idx = i
-    let chars = Array(s)
-    if idx < 0 { idx += chars.count }
-    if idx < 0 || idx >= chars.count { fatalError("index out of range") }
-    return String(chars[idx])
-}
-
-func _sliceString(_ s: String, _ i: Int, _ j: Int) -> String {
-    var start = i
-    var end = j
-    let chars = Array(s)
-    let n = chars.count
-    if start < 0 { start += n }
-    if end < 0 { end += n }
-    if start < 0 { start = 0 }
-    if end > n { end = n }
-    if end < start { end = start }
-    return String(chars[start..<end])
+	var idx = i
+	let chars = Array(s)
+	if idx < 0 { idx += chars.count }
+	if idx < 0 || idx >= chars.count { fatalError("index out of range") }
+	return String(chars[idx])
 }
 
 func expand(_ s: String, _ left: Int, _ right: Int) -> Int {
-    var l = left
-    var r = right
-    let n = s.count
-    while l >= 0 && r < n {
-        if _indexString(s, l) != _indexString(s, r) {
-            break
-        }
-        l -= 1
-        r += 1
-    }
-    return r - l - 1
+	let s = s
+	let left = left
+	let right = right
+	
+	var l = left
+	var r = right
+	let n = s.count
+	while l >= 0 && r < n {
+		if _indexString(s, l) != _indexString(s, r) {
+			break
+		}
+		l = l - 1
+		r = r + 1
+	}
+	return r - l - 1
 }
 
 func longestPalindrome(_ s: String) -> String {
-    if s.count <= 1 {
-        return s
-    }
-    var start = 0
-    var end = 0
-    let n = s.count
-    for i in 0..<n {
-        let len1 = expand(s, i, i)
-        let len2 = expand(s, i, i + 1)
-        var l = len1
-        if len2 > len1 { l = len2 }
-        if l > end - start {
-            start = i - (l - 1) / 2
-            end = i + l / 2
-        }
-    }
-    return _sliceString(s, start, end + 1)
-}
-
-func example_1() {
-    let ans = longestPalindrome("babad")
-    assert(ans == "bab" || ans == "aba")
-}
-func example_2() { assert(longestPalindrome("cbbd") == "bb") }
-func single_char() { assert(longestPalindrome("a") == "a") }
-func two_chars() {
-    let ans = longestPalindrome("ac")
-    assert(ans == "a" || ans == "c")
+	let s = s
+	
+	if s.count <= 1 {
+		return s
+	}
+	var start = 0
+	var end = 0
+	let n = s.count
+	for i in 0..<n {
+		let len1 = expand(s, i, i)
+		let len2 = expand(s, i, i + 1)
+		var l = len1
+		if len2 > len1 {
+			l = len2
+		}
+		if l > (end - start) {
+			start = i - ((l - 1) / 2)
+			end = i + (l / 2)
+		}
+	}
+	var res = ""
+	var k = start
+	while k <= end {
+		res = res + _indexString(s, k)
+		k = k + 1
+	}
+	return res
 }
 
 func main() {
-    example_1()
-    example_2()
-    single_char()
-    two_chars()
 }
 main()
