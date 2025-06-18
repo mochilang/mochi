2ce776830fa8aa4657b7168c36373c6801bc0a18
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-18 23:09:38 +0700

    Mention Dart output in leetcode README

---
+++ examples/leetcode-out/dart/5/longest-palindromic-substring.dart
@@ -0,0 +1,38 @@
+dynamic expand(s, left, right) {
+	dynamic l = left;
+	dynamic r = right;
+	dynamic n = s.length;
+	while (((l >= 0) && (r < n))) {
+		if ((s[l] != s[r])) {
+			break;
+		}
+		l = (l - 1);
+		r = (r + 1);
+	}
+	return ((r - l) - 1);
+}
+
+dynamic longestPalindrome(s) {
+	if ((s.length <= 1)) {
+		return s;
+	}
+	dynamic start = 0;
+	dynamic end = 0;
+	dynamic n = s.length;
+	for (var i = 0; i < n; i++) {
+		dynamic len1 = expand(s, i, i);
+		dynamic len2 = expand(s, i, (i + 1));
+		dynamic l = len1;
+		if ((len2 > len1)) {
+			l = len2;
+		}
+		if ((l > (end - start))) {
+			start = (i - (((l - 1)) ~/ 2));
+			end = (i + (l ~/ 2));
+		}
+	}
+	return s.sublist(start, (end + 1));
+}
+
+void main() {
+}
