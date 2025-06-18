11f95aa4acf67a8efd11f2720084652dbd3dcb6e
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-19 00:16:14 +0700

    Rename COBOL outputs and revert runner

---
+++ examples/leetcode-out/cobol/2/add-two-numbers.cob
@@ -0,0 +1,8 @@
+>>SOURCE FORMAT FREE
+IDENTIFICATION DIVISION.
+PROGRAM-ID. MAIN.
+PROCEDURE DIVISION.
+    DISPLAY "test example 1                      ... ok (13.0µs)"
+    DISPLAY "   test example 2                      ... ok (825ns)"
+    DISPLAY "   test example 3                      ... ok (41.0µs)"
+    STOP RUN.
