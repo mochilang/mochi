5e39d4070bf193f4811f077a9dd6110bc65c8d99
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-18 13:13:16 +0700

    compiler/swift: support function literals

--- compile/swift/helpers.go
+++ compile/swift/helpers.go
@@ -1,5 +1,7 @@
 package swiftcode
 
+import "strings"
+
 func (c *Compiler) writeln(s string) {
 	c.writeIndent()
 	c.buf.WriteString(s)
@@ -10,4 +12,16 @@ func (c *Compiler) writeIndent() {
 	for i := 0; i < c.indent; i++ {
 		c.buf.WriteByte('\t')
 	}
+}
+
+func indentBlock(s string, depth int) string {
+	if s == "" {
+		return s
+	}
+	prefix := strings.Repeat("\t", depth)
+	lines := strings.Split(strings.TrimRight(s, "\n"), "\n")
+	for i, line := range lines {
+		lines[i] = prefix + line
+	}
+	return strings.Join(lines, "\n") + "\n"
 }
