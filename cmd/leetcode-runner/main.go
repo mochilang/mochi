cddbef49a75e1c2fbbd55a4f71eddb5c9fbe63d2
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-18 23:06:41 +0700

    Add Erlang runner support and compile first four LeetCode problems

--- cmd/leetcode-runner/main.go
+++ cmd/leetcode-runner/main.go
@@ -358,6 +358,14 @@ func runOutput(file, lang string) error {
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		return cmd.Run()
+	case "erlang":
+		if err := erlcode.EnsureErlang(); err != nil {
+			return err
+		}
+		cmd := exec.Command("escript", file)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		return cmd.Run()
 	default:
 		return fmt.Errorf("no runner for %s", lang)
 	}
