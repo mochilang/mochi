91959e578b6f319163368a7f3958fc996635b50b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-19 16:27:57 +0700

    Update Prolog LeetCode outputs and golden files

--- examples/leetcode-out/pl/10/regular-expression-matching.pl
+++ examples/leetcode-out/pl/10/regular-expression-matching.pl
@@ -90,79 +90,90 @@ set_item(List, Index, Val, Out) :-
 										;
 										true
 										),
+										nb_setval(ismatch_star, false),
 										nb_getval(ismatch_j2, _V27),
-										_V31 is _V27 + 1,
-										nb_getval(ismatch_j2, _V28),
-										_V29 is _V28 + 1,
-										get_item(P, _V29, _V30),
-										((_V31 < N, _V30 =:= "*") ->
-											nb_getval(ismatch_dp, _V32),
-											nb_getval(ismatch_i2, _V33),
-											get_item(_V32, _V33, _V34),
-											nb_getval(ismatch_j2, _V35),
-											_V36 is _V35 + 2,
-											get_item(_V34, _V36, _V37),
-											nb_getval(ismatch_first, _V38),
-											nb_getval(ismatch_dp, _V39),
-											nb_getval(ismatch_i2, _V40),
-											_V41 is _V40 + 1,
-											get_item(_V39, _V41, _V42),
-											nb_getval(ismatch_j2, _V43),
-											get_item(_V42, _V43, _V44),
-											((_V37 ; (_V38, _V44)) ->
-												nb_getval(ismatch_dp, _V47),
-												nb_getval(ismatch_i2, _V45),
-												get_item(_V47, _V45, _V48),
-												nb_getval(ismatch_j2, _V46),
-												set_item(_V48, _V46, true, _V49),
-												set_item(_V47, _V45, _V49, _V50),
-												nb_setval(ismatch_dp, _V50)
+										_V28 is _V27 + 1,
+										(_V28 < N ->
+											nb_getval(ismatch_j2, _V29),
+											_V30 is _V29 + 1,
+											get_item(P, _V30, _V31),
+											(_V31 =:= "*" ->
+												nb_setval(ismatch_star, true)
 											;
-												nb_getval(ismatch_dp, _V53),
-												nb_getval(ismatch_i2, _V51),
-												get_item(_V53, _V51, _V54),
-												nb_getval(ismatch_j2, _V52),
-												set_item(_V54, _V52, false, _V55),
-												set_item(_V53, _V51, _V55, _V56),
-												nb_setval(ismatch_dp, _V56)
+											true
 											)
 										;
-											nb_getval(ismatch_first, _V57),
-											nb_getval(ismatch_dp, _V58),
-											nb_getval(ismatch_i2, _V59),
-											_V60 is _V59 + 1,
-											get_item(_V58, _V60, _V61),
-											nb_getval(ismatch_j2, _V62),
-											_V63 is _V62 + 1,
-											get_item(_V61, _V63, _V64),
-											((_V57, _V64) ->
-												nb_getval(ismatch_dp, _V67),
-												nb_getval(ismatch_i2, _V65),
-												get_item(_V67, _V65, _V68),
-												nb_getval(ismatch_j2, _V66),
-												set_item(_V68, _V66, true, _V69),
-												set_item(_V67, _V65, _V69, _V70),
-												nb_setval(ismatch_dp, _V70)
+										true
+										),
+										nb_getval(ismatch_star, _V32),
+										(_V32 ->
+											nb_getval(ismatch_dp, _V33),
+											nb_getval(ismatch_i2, _V34),
+											get_item(_V33, _V34, _V35),
+											nb_getval(ismatch_j2, _V36),
+											_V37 is _V36 + 2,
+											get_item(_V35, _V37, _V38),
+											nb_getval(ismatch_first, _V39),
+											nb_getval(ismatch_dp, _V40),
+											nb_getval(ismatch_i2, _V41),
+											_V42 is _V41 + 1,
+											get_item(_V40, _V42, _V43),
+											nb_getval(ismatch_j2, _V44),
+											get_item(_V43, _V44, _V45),
+											((_V38 ; (_V39, _V45)) ->
+												nb_getval(ismatch_dp, _V48),
+												nb_getval(ismatch_i2, _V46),
+												get_item(_V48, _V46, _V49),
+												nb_getval(ismatch_j2, _V47),
+												set_item(_V49, _V47, true, _V50),
+												set_item(_V48, _V46, _V50, _V51),
+												nb_setval(ismatch_dp, _V51)
 											;
-												nb_getval(ismatch_dp, _V73),
-												nb_getval(ismatch_i2, _V71),
-												get_item(_V73, _V71, _V74),
-												nb_getval(ismatch_j2, _V72),
-												set_item(_V74, _V72, false, _V75),
-												set_item(_V73, _V71, _V75, _V76),
-												nb_setval(ismatch_dp, _V76)
+												nb_getval(ismatch_dp, _V54),
+												nb_getval(ismatch_i2, _V52),
+												get_item(_V54, _V52, _V55),
+												nb_getval(ismatch_j2, _V53),
+												set_item(_V55, _V53, false, _V56),
+												set_item(_V54, _V52, _V56, _V57),
+												nb_setval(ismatch_dp, _V57)
+											)
+										;
+											nb_getval(ismatch_first, _V58),
+											nb_getval(ismatch_dp, _V59),
+											nb_getval(ismatch_i2, _V60),
+											_V61 is _V60 + 1,
+											get_item(_V59, _V61, _V62),
+											nb_getval(ismatch_j2, _V63),
+											_V64 is _V63 + 1,
+											get_item(_V62, _V64, _V65),
+											((_V58, _V65) ->
+												nb_getval(ismatch_dp, _V68),
+												nb_getval(ismatch_i2, _V66),
+												get_item(_V68, _V66, _V69),
+												nb_getval(ismatch_j2, _V67),
+												set_item(_V69, _V67, true, _V70),
+												set_item(_V68, _V66, _V70, _V71),
+												nb_setval(ismatch_dp, _V71)
+											;
+												nb_getval(ismatch_dp, _V74),
+												nb_getval(ismatch_i2, _V72),
+												get_item(_V74, _V72, _V75),
+												nb_getval(ismatch_j2, _V73),
+												set_item(_V75, _V73, false, _V76),
+												set_item(_V74, _V72, _V76, _V77),
+												nb_setval(ismatch_dp, _V77)
 											)
 										),
-										nb_getval(ismatch_j2, _V77),
-										_V78 is _V77 - 1,
-										nb_setval(ismatch_j2, _V78),
+										nb_getval(ismatch_j2, _V78),
+										_V79 is _V78 - 1,
+										nb_setval(ismatch_j2, _V79),
 										fail
 									; true)
 							)
 							, break, true),
-						nb_getval(ismatch_i2, _V79),
-						_V80 is _V79 - 1,
-						nb_setval(ismatch_i2, _V80),
+						nb_getval(ismatch_i2, _V80),
+						_V81 is _V80 - 1,
+						nb_setval(ismatch_i2, _V81),
 						fail
 					; true)
 			)
@@ -170,15 +181,15 @@ set_item(List, Index, Val, Out) :-
 					,
 					true
 				)
-				, return(_V81),
-					Res = _V81
+				, return(_V82),
+					Res = _V82
 				)
 			.
 			ismatch(S, P, Res) :-
-			nb_getval(ismatch_dp, _V82),
-			get_item(_V82, 0, _V83),
+			nb_getval(ismatch_dp, _V83),
 			get_item(_V83, 0, _V84),
-			Res = _V84.
+			get_item(_V84, 0, _V85),
+			Res = _V85.
 
 	main :- true.
 :- initialization(main, main).
