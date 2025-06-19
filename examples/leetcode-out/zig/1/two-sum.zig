3d405005aba576b86741ff3c34e93dbfdc0961b2
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-19 10:33:21 +0700

    zig backend: handle string ops

--- examples/leetcode-out/zig/1/two-sum.zig
+++ examples/leetcode-out/zig/1/two-sum.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 fn twoSum(nums: []const i32, target: i32) [2]i32 {
-	const n = nums.len;
+	const n = (nums).len;
 	for (0 .. n) |i| {
 		for ((i + 1) .. n) |j| {
 			if (((nums[i] + nums[j]) == target)) {
@@ -14,6 +14,6 @@ fn twoSum(nums: []const i32, target: i32) [2]i32 {
 
 pub fn main() void {
 	const result = twoSum(&[_]i32{@as(i32,@intCast(2)), @as(i32,@intCast(7)), @as(i32,@intCast(11)), @as(i32,@intCast(15))}, 9);
-	std.debug.print("{}\n", .{result[0]});
-	std.debug.print("{}\n", .{result[1]});
+	std.debug.print("{d}\n", .{result[0]});
+	std.debug.print("{d}\n", .{result[1]});
 }
--- examples/leetcode-out/zig/9/palindrome-number.zig
+++ examples/leetcode-out/zig/9/palindrome-number.zig
@@ -4,8 +4,8 @@ fn isPalindrome(x: i32) [2]i32 {
 	if ((x < 0)) {
 		return false;
 	}
-	const s = str(x);
-	const n = s.len;
+	const s = std.fmt.allocPrint(std.heap.page_allocator, "{d}", .{x}) catch unreachable;
+	const n = (s).len;
 	for (0 .. (n / 2)) |i| {
 		if ((s[i] != s[((n - 1) - i)])) {
 			return false;
