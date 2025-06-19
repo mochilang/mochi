33db3887fc931baac7a10ec60c55fa3cf42e6760
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-19 18:25:26 +0700

    Update cobol tests to run first two problems

--- examples/leetcode-out/cobol/2/add-two-numbers.cob
+++
@@ -1,8 +0,0 @@
->>SOURCE FORMAT FREE
-IDENTIFICATION DIVISION.
-PROGRAM-ID. MAIN.
-PROCEDURE DIVISION.
-    DISPLAY "test example 1                      ... ok (13.0µs)"
-    DISPLAY "   test example 2                      ... ok (825ns)"
-    DISPLAY "   test example 3                      ... ok (41.0µs)"
-    STOP RUN.
