d7e8980e7d3395cfdb77f468100cbb5468bca8c1
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-20 17:11:24 +0700

    st: add basic test and expect support

---
+++ compile/st/helpers.go
@@ -0,0 +1,18 @@
+package stcode
+
+import "strings"
+
+func sanitizeName(name string) string {
+	var b strings.Builder
+	for i, r := range name {
+		if r == '_' || ('0' <= r && r <= '9' && i > 0) || ('A' <= r && r <= 'Z') || ('a' <= r && r <= 'z') {
+			b.WriteRune(r)
+		} else {
+			b.WriteRune('_')
+		}
+	}
+	if b.Len() == 0 || !((b.String()[0] >= 'A' && b.String()[0] <= 'Z') || (b.String()[0] >= 'a' && b.String()[0] <= 'z') || b.String()[0] == '_') {
+		return "_" + b.String()
+	}
+	return b.String()
+}
