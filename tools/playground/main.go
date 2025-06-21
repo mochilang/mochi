8767efe69367542624c0768d003ead7c96fc5f92
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-22 02:00:39 +0700

    Add Makefile for playground

---
+++ tools/playground/main.go
@@ -0,0 +1,50 @@
+//go:build tinygo
+
+package main
+
+import (
+	"bytes"
+	"strings"
+	"syscall/js"
+
+	"mochi/interpreter"
+	"mochi/parser"
+	"mochi/runtime/mod"
+	"mochi/types"
+)
+
+func runMochi(this js.Value, args []js.Value) any {
+	if len(args) < 1 {
+		return js.ValueOf("missing source")
+	}
+	src := args[0].String()
+	prog, err := parser.ParseString(src)
+	if err != nil {
+		return js.ValueOf(err.Error())
+	}
+	env := types.NewEnv(nil)
+	var buf bytes.Buffer
+	env.SetWriter(&buf)
+	modRoot, errRoot := mod.FindRoot(".")
+	if errRoot != nil {
+		modRoot = "."
+	}
+	if errs := types.Check(prog, env); len(errs) > 0 {
+		var sb strings.Builder
+		for _, e := range errs {
+			sb.WriteString(e.Error())
+			sb.WriteByte('\n')
+		}
+		return js.ValueOf(sb.String())
+	}
+	interp := interpreter.New(prog, env, modRoot)
+	if err := interp.Run(); err != nil {
+		return js.ValueOf(err.Error())
+	}
+	return js.ValueOf(buf.String())
+}
+
+func main() {
+	js.Global().Set("runMochi", js.FuncOf(runMochi))
+	select {}
+}
