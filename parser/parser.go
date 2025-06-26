eec7358312dcd0de3b8ac0ffec3edc425955d11f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-26 08:44:50 +0700

    Add query let clause support

--- parser/parser.go
+++ parser/parser.go
@@ -416,6 +416,7 @@ type QueryExpr struct {
 	Froms    []*FromClause  `parser:"{ @@ }"`
 	Joins    []*JoinClause  `parser:"{ @@ }"`
 	Where    *Expr          `parser:"[ 'where' @@ ]"`
+	Lets     []*QueryLet    `parser:"{ @@ }"`
 	Group    *GroupByClause `parser:"[ @@ ]"`
 	Sort     *Expr          `parser:"[ ( 'sort' | 'order' ) 'by' @@ ]"`
 	Skip     *Expr          `parser:"[ 'skip' @@ ]"`
@@ -441,6 +442,12 @@ type JoinClause struct {
 	Var  string  `parser:"'join' [ 'from' ] @Ident 'in'"`
 	Src  *Expr   `parser:"@@"`
 	On   *Expr   `parser:"'on' @@"`
+}
+
+type QueryLet struct {
+	Pos   lexer.Position
+	Name  string `parser:"'let' @Ident '='"`
+	Value *Expr  `parser:"@@"`
 }
 
 type GroupByClause struct {
