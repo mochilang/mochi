374c9dd7e22978ab7e62596f748e758235fccd0c
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-27 21:58:34 +0700

    cobol: add tpch golden tests

--- compile/x/cobol/tpch_test.go
+++ compile/x/cobol/tpch_test.go
@@ -3,6 +3,7 @@
 package cobolcode_test
 
 import (
+	"fmt"
 	"testing"
 
 	cobolcode "mochi/compile/x/cobol"
@@ -15,7 +16,10 @@ func TestCobolCompiler_TPCH(t *testing.T) {
 	if err := cobolcode.EnsureCOBOL(); err != nil {
 		t.Skipf("cobol not installed: %v", err)
 	}
-	testutil.CompileTPCH(t, "q1", func(env *types.Env, prog *parser.Program) ([]byte, error) {
-		return cobolcode.New(env).Compile(prog)
-	})
+	for i := 1; i <= 2; i++ {
+		q := fmt.Sprintf("q%d", i)
+		testutil.CompileTPCH(t, q, func(env *types.Env, prog *parser.Program) ([]byte, error) {
+			return cobolcode.New(env).Compile(prog)
+		})
+	}
 }
