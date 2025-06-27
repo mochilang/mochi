1f3ca34216d8beefd721133edcebca175852a4df
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-27 10:53:32 +0700

    chore(ts): improve formatting of generated code

--- compile/ts/helpers.go
+++ compile/ts/helpers.go
@@ -30,9 +30,11 @@ func (c *Compiler) writeln(s string) {
 	c.buf.WriteByte('\n')
 }
 
+const indentStr = "  "
+
 func (c *Compiler) writeIndent() {
 	for i := 0; i < c.indent; i++ {
-		c.buf.WriteByte('\t')
+		c.buf.WriteString(indentStr)
 	}
 }
 
@@ -40,7 +42,7 @@ func indentBlock(s string, depth int) string {
 	if s == "" {
 		return s
 	}
-	prefix := strings.Repeat("\t", depth)
+	prefix := strings.Repeat(indentStr, depth)
 	lines := strings.Split(strings.TrimRight(s, "\n"), "\n")
 	for i, line := range lines {
 		lines[i] = prefix + line
