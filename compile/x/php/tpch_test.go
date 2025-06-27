95a98ab4bdf58e86cc2a21efc79d9d48b9e78c57
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-27 22:04:33 +0700

    php: support tpch q2 and golden tests

--- compile/x/php/tpch_test.go
+++ compile/x/php/tpch_test.go
@@ -3,6 +3,7 @@
 package phpcode_test
 
 import (
+	"fmt"
 	"testing"
 
 	phpcode "mochi/compile/x/php"
@@ -15,7 +16,10 @@ func TestPHPCompiler_TPCH(t *testing.T) {
 	if err := phpcode.EnsurePHP(); err != nil {
 		t.Skipf("php not installed: %v", err)
 	}
-	testutil.CompileTPCH(t, "q1", func(env *types.Env, prog *parser.Program) ([]byte, error) {
-		return phpcode.New(env).Compile(prog)
-	})
+	for i := 1; i <= 2; i++ {
+		q := fmt.Sprintf("q%d", i)
+		testutil.CompileTPCH(t, q, func(env *types.Env, prog *parser.Program) ([]byte, error) {
+			return phpcode.New(env).Compile(prog)
+		})
+	}
 }
