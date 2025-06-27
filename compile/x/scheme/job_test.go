b4d13717cdff43ae2471f9160cf6aeda55747272
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-06-27 19:57:46 +0700

    scheme: add JOB dataset tests

--- compile/x/scheme/job_test.go
+++ compile/x/scheme/job_test.go
@@ -15,7 +15,7 @@ func TestSchemeCompiler_JOB(t *testing.T) {
 	if _, err := schemecode.EnsureScheme(); err != nil {
 		t.Skipf("scheme not installed: %v", err)
 	}
-	for _, q := range []string{"q1", "q2"} {
+	for _, q := range []string{"q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10"} {
 		q := q
 		t.Run(q, func(t *testing.T) {
 			testutil.CompileJOB(t, q, func(env *types.Env, prog *parser.Program) ([]byte, error) {
