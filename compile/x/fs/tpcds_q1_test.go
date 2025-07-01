82643c2b08559b94c5d74fc41154aa4f5b874394
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-01 23:49:17 +0700

    fs compiler: support tpcds q30-q49

--- compile/x/fs/tpcds_q1_test.go
+++ compile/x/fs/tpcds_q1_test.go
@@ -26,7 +26,7 @@ func TestFSCompiler_TPCDS(t *testing.T) {
 		t.Skipf("fantomas not installed: %v", err)
 	}
 	root := testutil.FindRepoRoot(t)
-	for i := 1; i <= 29; i++ {
+	for i := 1; i <= 49; i++ {
 		q := fmt.Sprintf("q%d", i)
 		t.Run(q, func(t *testing.T) {
 			src := filepath.Join(root, "tests", "dataset", "tpc-ds", q+".mochi")
