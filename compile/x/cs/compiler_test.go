3b93f8d8d2c6c4b5b564dd3afcf71385c8cfa6b6
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-02 00:26:23 +0700

    cs: add tpcds q20-49 support

--- compile/x/cs/compiler_test.go
+++ compile/x/cs/compiler_test.go
@@ -220,7 +220,7 @@ func TestCSCompiler_TPCDSQueries(t *testing.T) {
 		t.Skipf("dotnet not runnable: %v", err)
 	}
 	root := findRepoRoot(t)
-	for i := 1; i <= 9; i++ {
+	for i := 1; i <= 49; i++ {
 		q := fmt.Sprintf("q%d", i)
 		t.Run(q, func(t *testing.T) {
 			src := filepath.Join(root, "tests", "dataset", "tpc-ds", q+".mochi")
