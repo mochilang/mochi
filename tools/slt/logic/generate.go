6ce03fe3706bb1943a5e362e96e7117d15d9d76a
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-02 22:05:36 +0700

    slt: place result flattening inside tests

--- tools/slt/logic/generate.go
+++ tools/slt/logic/generate.go
@@ -548,18 +548,19 @@ func Generate(c Case) string {
 			}
 		}
 		sb.WriteString("\n  select [" + strings.Join(exprs, ", ") + "]\n")
-		sb.WriteString("var flatResult = []\n")
-		sb.WriteString("for row in result {\n")
-		sb.WriteString("  for x in row {\n")
-		sb.WriteString("    flatResult = append(flatResult, x)\n")
-		sb.WriteString("  }\n}\n")
-		sb.WriteString("for x in flatResult {\n  print(x)\n}\n")
+		sb.WriteString(fmt.Sprintf("test \"%s\" {\n", c.Name))
+		sb.WriteString("  var flatResult = []\n")
+		sb.WriteString("  for row in result {\n")
+		sb.WriteString("    for x in row {\n")
+		sb.WriteString("      flatResult = append(flatResult, x)\n")
+		sb.WriteString("    }\n  }\n")
+		sb.WriteString("  for x in flatResult {\n    print(x)\n  }\n")
 		if len(c.Expect) > 0 {
-			sb.WriteString(fmt.Sprintf("test \"%s\" {\n  expect flatResult == %s\n}\n", c.Name, formatExpectList(c.Expect)))
+			sb.WriteString(fmt.Sprintf("  expect flatResult == %s\n", formatExpectList(c.Expect)))
 		} else {
-			sb.WriteString(fmt.Sprintf("test \"%s\" {\n  expect flatResult == []\n}\n", c.Name))
+			sb.WriteString("  expect flatResult == []\n")
 		}
-		sb.WriteString("\n")
+		sb.WriteString("}\n\n")
 		return sb.String()
 	}
 
