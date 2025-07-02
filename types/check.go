fc7582ffc715b9ad4ff3cfa9deb19d8eb25da659
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-02 21:35:41 +0700

    fix SLT generation for case9

--- types/check.go
+++ types/check.go
@@ -2013,11 +2013,11 @@ func checkQueryExpr(q *parser.QueryExpr, env *Env, expected Type) (Type, error) 
 	child := NewEnv(env)
 	child.SetVar(q.Var, elemT, true)
 
-	for _, f := range q.Froms {
-		ft, err := checkExpr(f.Src, env)
-		if err != nil {
-			return nil, err
-		}
+       for _, f := range q.Froms {
+               ft, err := checkExpr(f.Src, child)
+               if err != nil {
+                       return nil, err
+               }
 		var fe Type
 		switch t := ft.(type) {
 		case ListType:
@@ -2030,11 +2030,11 @@ func checkQueryExpr(q *parser.QueryExpr, env *Env, expected Type) (Type, error) 
 		child.SetVar(f.Var, fe, true)
 	}
 
-	for _, j := range q.Joins {
-		jt, err := checkExpr(j.Src, env)
-		if err != nil {
-			return nil, err
-		}
+       for _, j := range q.Joins {
+               jt, err := checkExpr(j.Src, child)
+               if err != nil {
+                       return nil, err
+               }
 		var je Type
 		switch t := jt.(type) {
 		case ListType:
