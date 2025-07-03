36e7b18e5cebca906120d4e5330bc1ef29e16733
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-03 19:17:56 +0700

    Generate select2 cases 900-1000

--- tools/slt/logic/generate.go
+++ tools/slt/logic/generate.go
@@ -864,15 +864,18 @@ func Generate(c Case) string {
 		}
 		expr := exprToMochi(ae.Expr, subs)
 		cond := condToMochi(sel.Where, subs)
-		sb.WriteString("let result = from row in " + tblNameStr)
+                sb.WriteString("var result = from row in " + tblNameStr)
 		if cond != "" {
 			sb.WriteString("\n  where " + cond)
 		}
 		if len(sel.OrderBy) == 1 {
 			sb.WriteString("\n  order by " + orderExprToMochi(sel.OrderBy[0].Expr, []*sqlparser.AliasedExpr{ae}, subs))
 		}
-		sb.WriteString("\n  select " + expr + "\n")
-		sb.WriteString("for x in result {\n  print(x)\n}\n\n")
+                sb.WriteString("\n  select " + expr + "\n")
+                if c.RowSort {
+                        sb.WriteString("result = from x in result\n  order by str(x)\n  select x\n")
+                }
+                sb.WriteString("for x in result {\n  print(x)\n}\n\n")
 		if len(c.Expect) > 0 {
 			sb.WriteString(fmt.Sprintf("test \"%s\" {\n  expect result == %s\n}\n", c.Name, formatExpectList(c.Expect)))
 		} else {
@@ -907,7 +910,7 @@ func Generate(c Case) string {
 			}
 		}
 		cond := condToMochi(sel.Where, subs)
-		sb.WriteString("let result = from row in " + tblNameStr)
+                sb.WriteString("var result = from row in " + tblNameStr)
 		if cond != "" {
 			sb.WriteString("\n  where " + cond)
 		}
@@ -927,12 +930,15 @@ func Generate(c Case) string {
 			}
 		}
 		sb.WriteString("\n  select [" + strings.Join(exprs, ", ") + "]\n")
-		sb.WriteString("var flatResult = []\n")
-		sb.WriteString("for row in result {\n")
-		sb.WriteString("  for x in row {\n")
-		sb.WriteString("    flatResult = append(flatResult, x)\n")
-		sb.WriteString("  }\n}\n")
-		sb.WriteString("for x in flatResult {\n  print(x)\n}\n")
+                sb.WriteString("var flatResult = []\n")
+                sb.WriteString("for row in result {\n")
+                sb.WriteString("  for x in row {\n")
+                sb.WriteString("    flatResult = append(flatResult, x)\n")
+                sb.WriteString("  }\n}\n")
+                if c.RowSort {
+                        sb.WriteString("flatResult = from x in flatResult\n  order by str(x)\n  select x\n")
+                }
+               sb.WriteString("for x in flatResult {\n  print(x)\n}\n")
 		if len(c.Expect) > 0 {
 			sb.WriteString(fmt.Sprintf("test \"%s\" {\n  expect flatResult == %s\n}\n", c.Name, formatExpectList(c.Expect)))
 		} else {
