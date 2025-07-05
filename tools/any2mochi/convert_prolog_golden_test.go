e848140ae9bbe948f81d47026482334749057057
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-05 23:18:11 +0700

    any2mochi: add prolog converter

---
+++ tools/any2mochi/convert_prolog_golden_test.go
@@ -0,0 +1,19 @@
+//go:build slow
+
+package any2mochi
+
+import (
+    "path/filepath"
+    "testing"
+)
+
+func TestConvertProlog_Golden(t *testing.T) {
+    root := findRepoRoot(t)
+    runConvertGolden(t, filepath.Join(root, "tests/compiler/pl"), "*.pl.out", ConvertPrologFile, "prolog", ".mochi", ".error")
+}
+
+func TestConvertPrologCompile_Golden(t *testing.T) {
+    root := findRepoRoot(t)
+    runConvertCompileGolden(t, filepath.Join(root, "tests/compiler/pl"), "*.pl.out", ConvertPrologFile, "prolog", ".mochi", ".error")
+}
+
