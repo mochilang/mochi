60ac58bc50a7b82b2eb1ce2efb96240efd6cc482
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-05 14:32:32 +0700

    refactor any2mochi tests

--- tools/any2mochi/convert_typescript_golden_test.go
+++ tools/any2mochi/convert_typescript_golden_test.go
@@ -21,6 +21,6 @@ func TestConvertTypeScriptCompile_Golden(t *testing.T) {
 	root := findRepoRoot(t)
 	_ = gocode.EnsureGopls()
 	_ = tscode.EnsureTSLanguageServer()
-	runConvertCompileGolden(t, filepath.Join(root, "tests/compiler/ts"), "*.ts.out", ConvertTypeScriptFile, "ts", ".mochi.out", ".error")
-	runConvertCompileGolden(t, filepath.Join(root, "tests/compiler/ts_simple"), "*.ts.out", ConvertTypeScriptFile, "ts", ".mochi.out", ".error")
+	runConvertCompileGolden(t, filepath.Join(root, "tests/compiler/ts"), "*.ts.out", ConvertTypeScriptFile, "ts", ".mochi", ".error")
+	runConvertCompileGolden(t, filepath.Join(root, "tests/compiler/ts_simple"), "*.ts.out", ConvertTypeScriptFile, "ts", ".mochi", ".error")
 }
