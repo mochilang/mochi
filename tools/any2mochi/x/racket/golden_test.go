0013bdeb168992fdc01098a458644eedb917daf4
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-06 14:09:44 +0700

    update Racket conversion golden

--- tools/any2mochi/x/racket/golden_test.go
+++ tools/any2mochi/x/racket/golden_test.go
@@ -5,9 +5,11 @@ package racket
 import (
 	"path/filepath"
 	"testing"
+
+	any2mochi "mochi/tools/any2mochi"
 )
 
 func TestConvert_Golden(t *testing.T) {
-	root := findRepoRoot(t)
-	runConvertGolden(t, filepath.Join(root, "tests/compiler/rkt"), "*.rkt.out", ConvertFile, "rkt", ".mochi", ".error")
+	root := any2mochi.FindRepoRoot(t)
+	any2mochi.RunConvertGolden(t, filepath.Join(root, "tests/compiler/rkt"), "*.rkt.out", ConvertFile, "rkt", ".mochi", ".error")
 }
