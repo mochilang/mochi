2404c6e8b16cd62ab2df56dcd87f21ff02dc701b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-06 11:18:19 +0700

    fix build

--- tools/any2mochi/convert_hs_golden_test.go
+++ tools/any2mochi/convert_hs_golden_test.go
@@ -5,9 +5,11 @@ package any2mochi
 import (
 	"path/filepath"
 	"testing"
+
+	hs "mochi/tools/any2mochi/x/hs"
 )
 
 func TestConvertHs_Golden(t *testing.T) {
 	root := findRepoRoot(t)
-	runConvertGolden(t, filepath.Join(root, "tests/compiler/hs"), "*.hs.out", ConvertHsFile, "hs", ".mochi", ".error")
+	runConvertGolden(t, filepath.Join(root, "tests/compiler/hs"), "*.hs.out", hs.ConvertFile, "hs", ".mochi", ".error")
 }
