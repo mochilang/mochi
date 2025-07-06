28e0fe3d8a0d60def6f96cb10b2dcc635829cac4
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-06 09:03:26 +0700

    refactor any2mochi go converter

--- tools/any2mochi/convert_go_golden_test.go
+++ tools/any2mochi/convert_go_golden_test.go
@@ -7,16 +7,17 @@ import (
 	"testing"
 
 	gocode "mochi/compile/go"
+	goconv "mochi/tools/any2mochi/go"
 )
 
 func TestConvertGo_Golden(t *testing.T) {
 	root := findRepoRoot(t)
 	_ = gocode.EnsureGopls()
-	runConvertGolden(t, filepath.Join(root, "tests/compiler/go"), "*.go.out", ConvertGoFile, "go", ".mochi", ".error")
+	runConvertGolden(t, filepath.Join(root, "tests/compiler/go"), "*.go.out", goconv.ConvertGoFile, "go", ".mochi", ".error")
 }
 
 func TestConvertGoCompile_Golden(t *testing.T) {
 	root := findRepoRoot(t)
 	_ = gocode.EnsureGopls()
-	runConvertCompileGolden(t, filepath.Join(root, "tests/compiler/go"), "*.go.out", ConvertGoFile, "go", ".mochi", ".error")
+	runConvertCompileGolden(t, filepath.Join(root, "tests/compiler/go"), "*.go.out", goconv.ConvertGoFile, "go", ".mochi", ".error")
 }
--- tools/any2mochi/go/ast.go
+++ tools/any2mochi/go/ast.go
@@ -1,4 +1,4 @@
-package any2mochi
+package goconv
 
 import (
 	"encoding/json"
