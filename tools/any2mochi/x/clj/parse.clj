5778d138322724080477e45c9a26ecb6f2607508
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-06 16:09:41 +0700

    clj converter fallback

--- tools/any2mochi/x/clj/parse.clj
+++ tools/any2mochi/x/clj/parse.clj
@@ -3,25 +3,33 @@
             [clojure.data.json :as json]))
 
 (defn node [expr]
-  (if (sequential? expr)
-    {:list (mapv node expr)}
-    {:atom (str expr)}))
+  (let [m (meta expr)]
+    (cond
+      (sequential? expr)
+      {:list (mapv node expr)
+       :line (:line m)
+       :col (:column m)}
+      :else
+      {:atom (str expr)
+       :line (:line m)
+       :col (:column m)})))
 
 (defn param-list [p]
   (mapv str p))
 
 (defn build-form [expr]
-  (if (and (sequential? expr) (seq expr))
-    (let [[head & rest] expr]
-      (cond
-        (= head 'defn)
-        (let [[name params & body] rest]
-          {:type "defn" :name (str name) :params (param-list params) :body (mapv node body)})
-        (= head 'def)
-        (let [[name value] rest]
-          {:type "def" :name (str name) :value (node value)})
-        :else {:type "expr" :body [(node expr)]}))
-    {:type "expr" :body [(node expr)]}))
+  (let [m (meta expr)]
+    (if (and (sequential? expr) (seq expr))
+      (let [[head & rest] expr]
+        (cond
+          (= head 'defn)
+          (let [[name params & body] rest]
+            {:type "defn" :name (str name) :params (param-list params) :body (mapv node body) :line (:line m) :col (:column m)})
+          (= head 'def)
+          (let [[name value] rest]
+            {:type "def" :name (str name) :value (node value) :line (:line m) :col (:column m)})
+          :else {:type "expr" :body [(node expr)] :line (:line m) :col (:column m)}))
+      {:type "expr" :body [(node expr)] :line (:line m) :col (:column m)}))
 
 (defn parse-file [f]
   (with-open [r (java.io.PushbackReader. (java.io.FileReader. f))]
