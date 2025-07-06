260c48a957b3c6c248a96e207220f257c3098b73
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-06 18:42:25 +0700

    test: unify convert golden

--- tools/any2mochi/x/racket/golden_test.go
+++ tools/any2mochi/x/racket/golden_test.go
@@ -11,5 +11,6 @@ import (
 
 func TestConvert_Golden(t *testing.T) {
 	root := any2mochi.FindRepoRoot(t)
-	any2mochi.RunConvertGolden(t, filepath.Join(root, "tests/compiler/rkt"), "*.rkt.out", ConvertFile, "rkt", ".mochi", ".error")
+	errs := any2mochi.RunConvertRunGolden(t, filepath.Join(root, "tests/compiler/rkt"), "*.rkt.out", ConvertFile, "rkt", ".mochi", ".error")
+	any2mochi.WriteErrorsMarkdown(filepath.Join(root, "tests/any2mochi/rkt"), errs)
 }
