3d316c79660ab529844bb6d4c5d0ba33eb2cccd4
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-06 11:16:04 +0700

    refactor: move fortran converter to subpackage

--- tools/any2mochi/convert_fortran_golden_test.go
+++ tools/any2mochi/convert_fortran_golden_test.go
@@ -5,9 +5,11 @@ package any2mochi
 import (
 	"path/filepath"
 	"testing"
+
+	ft "mochi/tools/any2mochi/x/fortran"
 )
 
 func TestConvertFortran_Golden(t *testing.T) {
 	root := findRepoRoot(t)
-	runConvertGolden(t, filepath.Join(root, "tests/compiler/fortran"), "*.f90.out", ConvertFortranFile, "fortran", ".mochi", ".error")
+	runConvertGolden(t, filepath.Join(root, "tests/compiler/fortran"), "*.f90.out", ft.ConvertFile, "fortran", ".mochi", ".error")
 }
