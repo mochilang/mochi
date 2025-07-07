11730ee267857b11c559bb4abe1cf08dbd32e88b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-07 21:07:13 +0700

    Add basic Go compiler and tests

---
+++ compiler/x/go/compiler.go
@@ -0,0 +1,187 @@
+package gocode
+
+import (
+	"bytes"
+	"fmt"
+
+	"mochi/parser"
+)
+
+// Compiler translates a subset of Mochi to Go source code.
+type Compiler struct {
+	buf    bytes.Buffer
+	indent int
+}
+
+// New creates a new Go compiler.
+func New() *Compiler {
+	return &Compiler{}
+}
+
+// Compile translates the given program to Go.
+func (c *Compiler) Compile(prog *parser.Program) ([]byte, error) {
+	c.buf.Reset()
+	c.indent = 0
+	c.writeln("//go:build ignore")
+	c.writeln("")
+	c.writeln("package main")
+	c.writeln("")
+	c.writeln("import \"fmt\"")
+	c.writeln("")
+	c.writeln("func main() {")
+	c.indent++
+	for _, s := range prog.Statements {
+		if err := c.compileStmt(s); err != nil {
+			return nil, err
+		}
+	}
+	c.indent--
+	c.writeln("}")
+	return c.buf.Bytes(), nil
+}
+
+func (c *Compiler) compileStmt(s *parser.Statement) error {
+	switch {
+	case s.Let != nil:
+		return c.compileLet(s.Let)
+	case s.Expr != nil:
+		expr, err := c.compileExpr(s.Expr.Expr)
+		if err != nil {
+			return err
+		}
+		c.writeln(expr)
+		return nil
+	default:
+		return fmt.Errorf("unsupported statement at line %d", s.Pos.Line)
+	}
+}
+
+func (c *Compiler) compileLet(l *parser.LetStmt) error {
+	if l.Value == nil {
+		return fmt.Errorf("let without value at line %d", l.Pos.Line)
+	}
+	val, err := c.compileExpr(l.Value)
+	if err != nil {
+		return err
+	}
+	c.writeln(fmt.Sprintf("%s := %s", l.Name, val))
+	return nil
+}
+
+func (c *Compiler) compileExpr(e *parser.Expr) (string, error) {
+	if e == nil || e.Binary == nil {
+		return "", fmt.Errorf("empty expr")
+	}
+	return c.compileBinary(e.Binary)
+}
+
+func (c *Compiler) compileBinary(b *parser.BinaryExpr) (string, error) {
+	left, err := c.compileUnary(b.Left)
+	if err != nil {
+		return "", err
+	}
+	res := left
+	for _, op := range b.Right {
+		r, err := c.compilePostfix(op.Right)
+		if err != nil {
+			return "", err
+		}
+		res = fmt.Sprintf("%s %s %s", res, op.Op, r)
+	}
+	return res, nil
+}
+
+func (c *Compiler) compileUnary(u *parser.Unary) (string, error) {
+	val, err := c.compilePostfix(u.Value)
+	if err != nil {
+		return "", err
+	}
+	for i := len(u.Ops) - 1; i >= 0; i-- {
+		val = u.Ops[i] + val
+	}
+	return val, nil
+}
+
+func (c *Compiler) compilePostfix(p *parser.PostfixExpr) (string, error) {
+	val, err := c.compilePrimary(p.Target)
+	if err != nil {
+		return "", err
+	}
+	if len(p.Ops) > 0 {
+		return "", fmt.Errorf("postfix operations not supported")
+	}
+	return val, nil
+}
+
+func (c *Compiler) compilePrimary(p *parser.Primary) (string, error) {
+	switch {
+	case p.Lit != nil:
+		return c.compileLiteral(p.Lit), nil
+	case p.List != nil:
+		elems := make([]string, len(p.List.Elems))
+		for i, e := range p.List.Elems {
+			s, err := c.compileExpr(e)
+			if err != nil {
+				return "", err
+			}
+			elems[i] = s
+		}
+		return "[]int{" + join(elems, ", ") + "}", nil
+	case p.Call != nil:
+		return c.compileCall(p.Call)
+	default:
+		return "", fmt.Errorf("unsupported expression at line %d", p.Pos.Line)
+	}
+}
+
+func (c *Compiler) compileCall(call *parser.CallExpr) (string, error) {
+	args := make([]string, len(call.Args))
+	for i, a := range call.Args {
+		s, err := c.compileExpr(a)
+		if err != nil {
+			return "", err
+		}
+		args[i] = s
+	}
+	argStr := join(args, ", ")
+	switch call.Func {
+	case "print":
+		return fmt.Sprintf("fmt.Println(%s)", argStr), nil
+	default:
+		return "", fmt.Errorf("unsupported function %s", call.Func)
+	}
+}
+
+func (c *Compiler) compileLiteral(l *parser.Literal) string {
+	switch {
+	case l.Int != nil:
+		return fmt.Sprintf("%d", *l.Int)
+	case l.Str != nil:
+		return fmt.Sprintf("%q", *l.Str)
+	case l.Float != nil:
+		return fmt.Sprintf("%g", *l.Float)
+	case l.Bool != nil:
+		return fmt.Sprintf("%t", bool(*l.Bool))
+	default:
+		return "nil"
+	}
+}
+
+func (c *Compiler) writeln(s string) {
+	for i := 0; i < c.indent; i++ {
+		c.buf.WriteString("    ")
+	}
+	c.buf.WriteString(s)
+	c.buf.WriteByte('\n')
+}
+
+func join(parts []string, sep string) string {
+	if len(parts) == 0 {
+		return ""
+	}
+	out := parts[0]
+	for _, p := range parts[1:] {
+		out += sep + p
+	}
+	return out
+}
