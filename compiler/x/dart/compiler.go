4ffc42884b689465d8f7df6c6600c17d676d232f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-07 23:31:25 +0700

    Implement basic Dart compiler

--- compiler/x/dart/compiler.go
+++ compiler/x/dart/compiler.go
@@ -1,6 +1,8 @@
 package dart
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -9,24 +11,250 @@ import (
 	"mochi/types"
 )
 
+// Compiler is a very small proof-of-concept translator that converts a limited
+// subset of Mochi programs into Dart code.  For many of the test programs this
+// translation is incomplete, but it is sufficient for simple examples such as
+// variable declarations and basic arithmetic/print statements.
 type Compiler struct {
-	env *types.Env
+	env    *types.Env
+	buf    bytes.Buffer
+	indent int
 }
 
-func New(env *types.Env) *Compiler {
-	return &Compiler{env: env}
-}
+// New creates a new Dart compiler instance.
+func New(env *types.Env) *Compiler { return &Compiler{env: env} }
 
+// Compile translates the given Mochi program into Dart source code.  If there
+// is a hand written translation under tests/human/x/dart it is returned.
+// Otherwise the program is compiled using the small subset supported here.
 func (c *Compiler) Compile(prog *parser.Program) ([]byte, error) {
 	src := prog.Pos.Filename
-	base := strings.TrimSuffix(filepath.Base(src), filepath.Ext(src))
-	root := findRepoRoot()
-	ref := filepath.Join(root, "tests", "human", "x", "dart", base+".dart")
-	if data, err := os.ReadFile(ref); err == nil {
-		return data, nil
+	_ = src
+
+	c.buf.Reset()
+	c.indent = 0
+	c.writeln("void main() {")
+	c.indent++
+	for _, st := range prog.Statements {
+		if err := c.compileStmt(st); err != nil {
+			return nil, err
+		}
 	}
-	// default stub
-	return []byte("void main() {}\n"), nil
+	c.indent--
+	c.writeln("}")
+	return c.buf.Bytes(), nil
+}
+
+func (c *Compiler) compileStmt(s *parser.Statement) error {
+	switch {
+	case s.Let != nil:
+		return c.compileLet(s.Let)
+	case s.Var != nil:
+		return c.compileVar(s.Var)
+	case s.Expr != nil:
+		expr, err := c.compileExpr(s.Expr.Expr)
+		if err != nil {
+			return err
+		}
+		c.writeln(expr + ";")
+		return nil
+	default:
+		return fmt.Errorf("unsupported statement at line %d", s.Pos.Line)
+	}
+}
+
+func (c *Compiler) compileLet(l *parser.LetStmt) error {
+	typ := dartType(l.Type)
+	var val string
+	var err error
+	if l.Value != nil {
+		val, err = c.compileExpr(l.Value)
+		if err != nil {
+			return err
+		}
+	} else {
+		val = defaultValue(typ)
+	}
+	if typ == "" {
+		c.writeln(fmt.Sprintf("var %s = %s;", l.Name, val))
+	} else {
+		c.writeln(fmt.Sprintf("%s %s = %s;", typ, l.Name, val))
+	}
+	return nil
+}
+
+func (c *Compiler) compileVar(v *parser.VarStmt) error {
+	typ := dartType(v.Type)
+	var val string
+	var err error
+	if v.Value != nil {
+		val, err = c.compileExpr(v.Value)
+		if err != nil {
+			return err
+		}
+	} else {
+		val = defaultValue(typ)
+	}
+	if typ == "" {
+		c.writeln(fmt.Sprintf("var %s = %s;", v.Name, val))
+	} else {
+		c.writeln(fmt.Sprintf("%s %s = %s;", typ, v.Name, val))
+	}
+	return nil
+}
+
+func (c *Compiler) compileExpr(e *parser.Expr) (string, error) {
+	if e == nil || e.Binary == nil {
+		return "", fmt.Errorf("empty expression")
+	}
+	return c.compileBinary(e.Binary)
+}
+
+func (c *Compiler) compileBinary(b *parser.BinaryExpr) (string, error) {
+	left, err := c.compileUnary(b.Left)
+	if err != nil {
+		return "", err
+	}
+	res := left
+	for _, op := range b.Right {
+		r, err := c.compilePostfix(op.Right)
+		if err != nil {
+			return "", err
+		}
+		res = fmt.Sprintf("%s %s %s", res, op.Op, r)
+	}
+	return res, nil
+}
+
+func (c *Compiler) compileUnary(u *parser.Unary) (string, error) {
+	val, err := c.compilePostfix(u.Value)
+	if err != nil {
+		return "", err
+	}
+	for i := len(u.Ops) - 1; i >= 0; i-- {
+		val = u.Ops[i] + val
+	}
+	return val, nil
+}
+
+func (c *Compiler) compilePostfix(p *parser.PostfixExpr) (string, error) {
+	val, err := c.compilePrimary(p.Target)
+	if err != nil {
+		return "", err
+	}
+	if len(p.Ops) > 0 {
+		return "", fmt.Errorf("postfix operations not supported")
+	}
+	return val, nil
+}
+
+func (c *Compiler) compilePrimary(p *parser.Primary) (string, error) {
+	switch {
+	case p.Lit != nil:
+		return c.compileLiteral(p.Lit), nil
+	case p.List != nil:
+		elems := make([]string, len(p.List.Elems))
+		for i, e := range p.List.Elems {
+			s, err := c.compileExpr(e)
+			if err != nil {
+				return "", err
+			}
+			elems[i] = s
+		}
+		return "[" + strings.Join(elems, ", ") + "]", nil
+	case p.Selector != nil:
+		s := p.Selector.Root
+		if len(p.Selector.Tail) > 0 {
+			s += "." + strings.Join(p.Selector.Tail, ".")
+		}
+		return s, nil
+	case p.Call != nil:
+		return c.compileCall(p.Call)
+	case p.Group != nil:
+		e, err := c.compileExpr(p.Group)
+		if err != nil {
+			return "", err
+		}
+		return "(" + e + ")", nil
+	default:
+		return "", fmt.Errorf("unsupported expression at line %d", p.Pos.Line)
+	}
+}
+
+func (c *Compiler) compileCall(call *parser.CallExpr) (string, error) {
+	args := make([]string, len(call.Args))
+	for i, a := range call.Args {
+		s, err := c.compileExpr(a)
+		if err != nil {
+			return "", err
+		}
+		args[i] = s
+	}
+	return fmt.Sprintf("%s(%s)", call.Func, strings.Join(args, ", ")), nil
+}
+
+func (c *Compiler) compileLiteral(l *parser.Literal) string {
+	switch {
+	case l.Int != nil:
+		return fmt.Sprintf("%d", *l.Int)
+	case l.Float != nil:
+		return fmt.Sprintf("%g", *l.Float)
+	case l.Bool != nil:
+		if bool(*l.Bool) {
+			return "true"
+		}
+		return "false"
+	case l.Str != nil:
+		s := strings.ReplaceAll(*l.Str, "'", "\\'")
+		return "'" + strings.Trim(s, "\"") + "'"
+	case l.Null:
+		return "null"
+	default:
+		return "null"
+	}
+}
+
+func (c *Compiler) writeln(s string) {
+	for i := 0; i < c.indent; i++ {
+		c.buf.WriteString("  ")
+	}
+	c.buf.WriteString(s)
+	c.buf.WriteByte('\n')
+}
+
+func dartType(t *parser.TypeRef) string {
+	if t == nil {
+		return ""
+	}
+	if t.Simple != nil {
+		switch *t.Simple {
+		case "int":
+			return "int"
+		case "float":
+			return "double"
+		case "string":
+			return "String"
+		case "bool":
+			return "bool"
+		}
+		return *t.Simple
+	}
+	return ""
+}
+
+func defaultValue(typ string) string {
+	switch typ {
+	case "int":
+		return "0"
+	case "double":
+		return "0.0"
+	case "String":
+		return "''"
+	case "bool":
+		return "false"
+	default:
+		return "null"
+	}
 }
 
 func findRepoRoot() string {
