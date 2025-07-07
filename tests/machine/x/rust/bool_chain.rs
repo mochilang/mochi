fab3791a197af46a3f56ddfd73272d33dad789e4
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-08 00:56:48 +0700

    update short_circuit return type

---
+++ tests/machine/x/rust/bool_chain.rs
@@ -0,0 +1,9 @@
+fn main() {
+    fn boom() -> bool {
+        println!("{:?}", "boom");
+        return true;
+    }
+    println!("{:?}", (1 < 2) && (2 < 3) && (3 < 4));
+    println!("{:?}", (1 < 2) && (2 > 3) && boom());
+    println!("{:?}", (1 < 2) && (2 < 3) && (3 > 4) && boom());
+}
