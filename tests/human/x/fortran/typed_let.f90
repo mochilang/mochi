847ab2b3a92556ee598a4ab91286bdfcddbb5646
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-07 08:09:00 +0700

    Add more Fortran translations and README

---
+++ tests/human/x/fortran/typed_let.f90
@@ -0,0 +1,5 @@
+program typed_let
+  implicit none
+  integer :: y
+  print *, y
+end program typed_let
---
+++ tests/human/x/fortran/unary_neg.f90
@@ -0,0 +1,5 @@
+program unary_neg
+  implicit none
+  print *, -3
+  print *, 5 + (-2)
+end program unary_neg
