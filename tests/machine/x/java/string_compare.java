529c6143245e9f999d1856ed5896fcf160472a6b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-07 21:33:27 +0700

    Add prototype Java compiler and test harness

---
+++ tests/machine/x/java/string_compare.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("a" < "b");
+		System.out.println("a" <= "a");
+		System.out.println("b" > "a");
+		System.out.println("b" >= "b");
+	}
+}
