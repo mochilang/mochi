4fe90e1f3efa88bb1d3756423458d52e5f1eb5e2
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-08 17:24:31 +0700

    Add OCaml compiler support and tests

---
+++ tests/machine/x/ocaml/string_prefix_slice.ml
@@ -0,0 +1,24 @@
+let rec __show v =
+  let open Obj in
+  let rec list_aux o =
+    if is_int o && (magic (obj o) : int) = 0 then "" else
+     let hd = field o 0 in
+     let tl = field o 1 in
+     let rest = list_aux tl in
+     if rest = "" then __show (obj hd) else __show (obj hd) ^ "; " ^ rest
+  in
+  let r = repr v in
+  if is_int r then string_of_int (magic v) else
+  match tag r with
+    | 0 -> if size r = 0 then "[]" else "[" ^ list_aux r ^ "]"
+    | 252 -> (magic v : string)
+    | 253 -> string_of_float (magic v)
+    | _ -> "<value>"
+
+let prefix = "fore"
+let s1 = "forest"
+let s2 = "desert"
+
+let () =
+  print_endline (__show ((List.nth s1 0 = prefix)));
+  print_endline (__show ((List.nth s2 0 = prefix)));
