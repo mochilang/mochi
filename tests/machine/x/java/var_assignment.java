94b12408eea3418fb6c010a6acef7ed484e90970
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-08 17:39:05 +0700

    Add Java compiler outputs for remaining samples

---
+++ tests/machine/x/java/var_assignment.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+	static int x = 1;
+	public static void main(String[] args) {
+	x = 2;
+	System.out.println(x);
+	}
+}
---
+++ tests/machine/x/java/while_loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+	static int i = 0;
+	public static void main(String[] args) {
+	while (i < 3) {
+		System.out.println(i);
+		i = i + 1;
+	}
+	}
+}
