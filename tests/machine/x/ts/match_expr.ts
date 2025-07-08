b96fffed038cdcc923869461241fdd08ae22b2a6
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-08 15:39:15 +0700

    ts tests: remove obsolete error files

---
+++ tests/machine/x/ts/match_expr.ts
@@ -0,0 +1,22 @@
+let x = 2;
+let label = (() => {
+  const _tmp1 = x;
+  let _res;
+  switch (_tmp1) {
+    case 1:
+      _res = "one";
+      break;
+    case 2:
+      _res = "two";
+      break;
+    case 3:
+      _res = "three";
+      break;
+    default:
+      _res = "unknown";
+      break;
+  }
+  return _res;
+})()
+;
+console.log(label);
