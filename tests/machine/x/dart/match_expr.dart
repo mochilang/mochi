a2055b77878c4a201be78456a38f213fa3807e59
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-09 19:02:01 +0700

    dart: add basic match expression support

---
+++ tests/machine/x/dart/match_expr.dart
@@ -0,0 +1,19 @@
+var x = 2;
+
+var label = (() {
+  var _t = x;
+  if (_t == 1) {
+    return 'one';
+  } else if (_t == 2) {
+    return 'two';
+  } else if (_t == 3) {
+    return 'three';
+  }  else {
+    return 'unknown';
+  }
+  return null;
+})();
+
+void main() {
+  print(label);
+}
