7fa13995adde5074605e70fd8187396ded1fd83d
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-09 15:26:30 +0700

    Add Go compiler test README generation

---
+++ tests/machine/x/go/fun_call.go
@@ -0,0 +1,16 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+// line 1
+func add(a int, b int) int {
+	return (a + b)
+}
+
+func main() {
+	fmt.Println(5)
+}
---
+++ tests/machine/x/go/fun_three_args.go
@@ -0,0 +1,16 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+// line 1
+func sum3(a int, b int, c int) int {
+	return ((a + b) + c)
+}
+
+func main() {
+	fmt.Println(6)
+}
