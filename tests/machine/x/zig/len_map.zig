f3f2b9134134509c4011e3f5e1b9266e6295dbb7
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-09 10:27:41 +0700

    zig compiler: fix map literal, add len_map

--- tests/machine/x/zig/len_map.zig
+++ tests/machine/x/zig/len_map.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("{any}\n", .{blk0: { var m = std.AutoHashMap([]const u8, i32).init(std.heap.page_allocator); m.put("a", 1) catch unreachable; m.put("b", 2) catch unreachable; break :blk0 m; }.count()});
+    std.debug.print("{d}\n", .{(blk0: { var m = std.AutoHashMap([]const u8, i32).init(std.heap.page_allocator); m.put("a", 1) catch unreachable; m.put("b", 2) catch unreachable; break :blk0 m; }).count()});
 }
