5b0f5cf60f976f68b121718a1ca4dec669def61b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-09 18:03:46 +0700

    compiler/java: support nested functions

---
+++ tests/machine/x/java/nested_function.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+	static int outer(int x) {
+		java.util.function.IntUnaryOperator inner = y -> {
+			return x + y;
+		};
+		return inner.applyAsInt(5);
+	}
+	public static void main(String[] args) {
+	System.out.println(outer(3));
+	}
+}
