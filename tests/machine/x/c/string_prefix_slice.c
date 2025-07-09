a7a1f337526e5e2be7bcf5ab304f5d48c29b1e2c
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-09 17:26:30 +0700

    c backend: handle string contains and slice comparisons

--- tests/machine/x/c/string_prefix_slice.c
+++ tests/machine/x/c/string_prefix_slice.c
@@ -34,7 +34,7 @@ int main() {
     _b[_e - _s] = '\0';
     _b;
   });
-  printf("%s\n", ((_t1 == prefix)) ? "true" : "false");
+  printf("%s\n", ((strcmp(_t1, prefix) == 0)) ? "true" : "false");
   char *s2 = "desert";
   char *_t2 = ({
     int _len = strlen(s2);
@@ -55,6 +55,6 @@ int main() {
     _b[_e - _s] = '\0';
     _b;
   });
-  printf("%s\n", ((_t2 == prefix)) ? "true" : "false");
+  printf("%s\n", ((strcmp(_t2, prefix) == 0)) ? "true" : "false");
   return 0;
 }
