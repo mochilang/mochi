ff9349ca17edd1436db40b997001c00561641cf5
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-10 03:52:23 +0700

    rust compiler: fix expression type inference

--- tests/machine/x/rust/dataset_sort_take_limit.rs
+++ tests/machine/x/rust/dataset_sort_take_limit.rs
@@ -1,6 +1,12 @@
+#[derive(Default, Debug, Clone, PartialEq)]
+struct Product {
+    name: &'static str,
+    price: i32,
+}
+
 fn main() {
-    let products = vec![{ let mut m = std::collections::HashMap::new(); m.insert("name", "Laptop"); m.insert("price", 1500); m }, { let mut m = std::collections::HashMap::new(); m.insert("name", "Smartphone"); m.insert("price", 900); m }, { let mut m = std::collections::HashMap::new(); m.insert("name", "Tablet"); m.insert("price", 600); m }, { let mut m = std::collections::HashMap::new(); m.insert("name", "Monitor"); m.insert("price", 300); m }, { let mut m = std::collections::HashMap::new(); m.insert("name", "Keyboard"); m.insert("price", 100); m }, { let mut m = std::collections::HashMap::new(); m.insert("name", "Mouse"); m.insert("price", 50); m }, { let mut m = std::collections::HashMap::new(); m.insert("name", "Headphones"); m.insert("price", 200); m }];
-    let expensive = { let mut tmp1 = Vec::new();for &p in &products { let tmp2 = p; let tmp3 = -p.price; tmp1.push((tmp3, tmp2)); } tmp1.sort_by(|a,b| a.0.partial_cmp(&b.0).unwrap()); let mut tmp4 = Vec::new(); for p in tmp1 { tmp4.push(p.1); } let tmp4 = tmp4[1 as usize..(1 + 3) as usize].to_vec(); tmp4 };
+    let products = vec![Product { name: "Laptop", price: 1500 }, Product { name: "Smartphone", price: 900 }, Product { name: "Tablet", price: 600 }, Product { name: "Monitor", price: 300 }, Product { name: "Keyboard", price: 100 }, Product { name: "Mouse", price: 50 }, Product { name: "Headphones", price: 200 }];
+    let expensive = { let mut tmp1 = Vec::new();for p in &products { let tmp2 = p.clone(); let tmp3 = -p.price; tmp1.push((tmp3, tmp2)); } tmp1.sort_by(|a,b| a.0.partial_cmp(&b.0).unwrap()); let mut tmp4 = Vec::new(); for p in tmp1 { tmp4.push(p.1); } let tmp4 = tmp4[1 as usize..(1 + 3) as usize].to_vec(); tmp4 };
     println!("{:?}", "--- Top products (excluding most expensive) ---");
     for item in expensive {
         println!("{:?} {:?} {:?}", item.name, "costs $", item.price);
