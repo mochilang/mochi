aebae386a180162b345427c92ec1e03d3c568c66
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-10 01:04:01 +0700

    tests: regenerate update_stmt output

---
+++ tests/machine/x/cpp/update_stmt.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+struct __struct1 {
+  decltype(std::string("Alice")) name;
+  decltype(17) age;
+  decltype(std::string("minor")) status;
+};
+int main() {
+  struct Person {
+    std::string name;
+    int age;
+    std::string status;
+  };
+  auto people = std::vector<decltype(__struct1{std::string("Alice"), 17,
+                                               std::string("minor")})>{
+      __struct1{std::string("Alice"), 17, std::string("minor")},
+      __struct1{std::string("Bob"), 25, std::string("unknown")},
+      __struct1{std::string("Charlie"), 18, std::string("unknown")},
+      __struct1{std::string("Diana"), 16, std::string("minor")}};
+  for (auto &__tmp1 : people) {
+    if ((__tmp1.age >= 18)) {
+      __tmp1.status = std::string("adult");
+      __tmp1.age = (__tmp1.age + 1);
+    }
+  }
+  {
+    std::cout << std::boolalpha << std::string("ok");
+    std::cout << std::endl;
+  }
+  return 0;
+}
