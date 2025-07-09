8829581ab13afa954d175d075a498b8d55708f0b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-09 21:05:31 +0700

    Add basic group-by support to F# compiler

--- tests/machine/x/fs/dataset_sort_take_limit.fs
+++ tests/machine/x/fs/dataset_sort_take_limit.fs
@@ -3,8 +3,12 @@ open System
 exception Break
 exception Continue
 
-let products = [dict [(name, "Laptop"); (price, 1500)]; dict [(name, "Smartphone"); (price, 900)]; dict [(name, "Tablet"); (price, 600)]; dict [(name, "Monitor"); (price, 300)]; dict [(name, "Keyboard"); (price, 100)]; dict [(name, "Mouse"); (price, 50)]; dict [(name, "Headphones"); (price, 200)]]
-let expensive = [ for p in products doyield p ] |> List.sortByDescending (fun _ -> p.price) |> List.skip 1 |> List.take 3
+type Anon1 = {
+    name: string
+    price: int
+}
+let products = [{ name = "Laptop"; price = 1500 }; { name = "Smartphone"; price = 900 }; { name = "Tablet"; price = 600 }; { name = "Monitor"; price = 300 }; { name = "Keyboard"; price = 100 }; { name = "Mouse"; price = 50 }; { name = "Headphones"; price = 200 }]
+let expensive = [ for p in products do yield p ] |> List.sortByDescending (fun p -> p.price) |> List.skip 1 |> List.take 3
 printfn "%s" "--- Top products (excluding most expensive) ---"
 try
     for item in expensive do
@@ -12,3 +16,4 @@ try
             printfn "%s" (String.concat " " [string item.name; string "costs $"; string item.price])
         with Continue -> ()
 with Break -> ()
+
--- tests/machine/x/fs/group_by_having.fs
+++ tests/machine/x/fs/group_by_having.fs
@@ -1,8 +1,20 @@
 open System
+open System.Text.Json
 
 exception Break
 exception Continue
 
-let people = [dict [(name, "Alice"); (city, "Paris")]; dict [(name, "Bob"); (city, "Hanoi")]; dict [(name, "Charlie"); (city, "Paris")]; dict [(name, "Diana"); (city, "Hanoi")]; dict [(name, "Eve"); (city, "Paris")]; dict [(name, "Frank"); (city, "Hanoi")]; dict [(name, "George"); (city, "Paris")]]
-let big = [ for p in people doyield dict [(city, g.key); (num, List.length g)] ]
-printfn "%A" (json big)
+type Anon1 = {
+    name: string
+    city: string
+}
+type Anon2 = {
+    city: obj
+    num: obj
+}
+let people = [{ name = "Alice"; city = "Paris" }; { name = "Bob"; city = "Hanoi" }; { name = "Charlie"; city = "Paris" }; { name = "Diana"; city = "Hanoi" }; { name = "Eve"; city = "Paris" }; { name = "Frank"; city = "Hanoi" }; { name = "George"; city = "Paris" }]
+let big = [ for gKey, gItems in [ for p in people do yield p ] |> List.groupBy (fun p -> p.city) |> List.filter (fun (gKey, gItems) -> let g = {| key = gKey; items = gItems |} in List.length g >= 4) do
+    let g = {| key = gKey; items = gItems |}
+    yield { city = g.key; num = List.length g } ]
+printfn "%A" (JsonSerializer.Serialize(big))
+
