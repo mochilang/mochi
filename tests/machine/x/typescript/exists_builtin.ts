2d25762204f415974c24ee03079859b00c92ddc9
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-10 12:11:23 +0700

    add remaining tasks section

--- tests/machine/x/typescript/exists_builtin.ts
+++ tests/machine/x/typescript/exists_builtin.ts
@@ -1,12 +1,3 @@
 const data = [1, 2];
-const flag = ((() => {
-  const _tmp1 = [];
-  for (const x of data) {
-    if (!((x == 1))) continue;
-    _tmp1.push(x);
-  }
-  let res = _tmp1;
-  return res;
-})()
-.length > 0);
+const flag = data.some((x) => (x == 1));
 console.log(flag);
