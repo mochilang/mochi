f33c53a76c37cd65d31ac6cbc84a6a29a38a9f0b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-10 14:03:41 +0700

    zig: regenerate machine outputs

--- tests/machine/x/zig/math_ops.zig
+++ tests/machine/x/zig/math_ops.zig
@@ -2,6 +2,6 @@ const std = @import("std");
 
 pub fn main() void {
     std.debug.print("{d}\n", .{(6 * 7)});
-    std.debug.print("{d}\n", .{(7 / 2)});
+    std.debug.print("{any}\n", .{(7 / 2)});
     std.debug.print("{d}\n", .{@mod(7, 2)});
 }
