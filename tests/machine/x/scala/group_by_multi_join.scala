fa9d9b7de6ec30ee5ff7429dcb12213146ae19a7
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-10 18:06:45 +0700

    scala: handle query args in aggregator calls

--- tests/machine/x/scala/group_by_multi_join.scala
+++ tests/machine/x/scala/group_by_multi_join.scala
@@ -3,7 +3,7 @@ object group_by_multi_join {
   val suppliers = List(Map("id" -> (1), "nation" -> (1)), Map("id" -> (2), "nation" -> (2)))
   val partsupp = List(Map("part" -> (100), "supplier" -> (1), "cost" -> (10), "qty" -> (2)), Map("part" -> (100), "supplier" -> (2), "cost" -> (20), "qty" -> (1)), Map("part" -> (200), "supplier" -> (1), "cost" -> (5), "qty" -> (3)))
   val filtered = for { ps <- partsupp; s <- suppliers; if (s.id).asInstanceOf[Int] == ps("supplier"); n <- nations; if (n.id).asInstanceOf[Int] == s("nation"); if n("name") == "A" } yield Map("part" -> (ps("part")), "value" -> (ps("cost") * ps("qty")))
-  val grouped = ((for { x <- filtered } yield (x("part"), (x))).groupBy(_._1).map{ case(k,list) => (k, list.map(_._2)) }.toList).map{ case(gKey,gItems) => { val g = (gKey, gItems); Map("part" -> (g._1), "total" -> (for { r <- g._2 } yield r.value.sum)) } }.toList
+  val grouped = ((for { x <- filtered } yield (x("part"), (x))).groupBy(_._1).map{ case(k,list) => (k, list.map(_._2)) }.toList).map{ case(gKey,gItems) => { val g = (gKey, gItems); Map("part" -> (g._1), "total" -> ((for { r <- g._2 } yield r.value).sum)) } }.toList
   def main(args: Array[String]): Unit = {
     println((grouped))
   }
