f0bf9065025cf719ce15821622edcea1191e9c81
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-10 12:06:06 +0700

    cpp compiler: handle global vars and len builtin

--- tests/machine/x/cpp/pure_global_fold.cpp
+++ tests/machine/x/cpp/pure_global_fold.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
+auto k = 2;
+
 auto inc(auto x) { return (x + k); }
 
 int main() {
-  auto k = 2;
   {
     std::cout << std::boolalpha << inc(3);
     std::cout << std::endl;
