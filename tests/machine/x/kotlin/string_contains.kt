f3352ede9d3799d22a80f07c44ca2691653df885
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-10 16:27:34 +0700

    kotlin: handle string method selectors

--- tests/machine/x/kotlin/string_contains.kt
+++ tests/machine/x/kotlin/string_contains.kt
@@ -1,6 +1,6 @@
 val s = "catch"
 
 fun main() {
-    println((s as MutableMap<*, *>)["contains"]("cat"))
-    println((s as MutableMap<*, *>)["contains"]("dog"))
+    println(s.contains("cat"))
+    println(s.contains("dog"))
 }
