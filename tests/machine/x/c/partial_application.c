2240487f4de4b2ae74a0f810541e8c33db83df18
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-11 20:04:19 +0700

    c compiler: fix partial application generation

--- tests/machine/x/c/partial_application.c
+++ tests/machine/x/c/partial_application.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int _partial0_arg0;
 static int _partial0(int p0) { return add(_partial0_arg0, p0); }
 
-static int _partial0_arg0;
 int add(int a, int b) { return a + b; }
 
 int main() {
