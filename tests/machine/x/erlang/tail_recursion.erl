1e1d4a0ca4c886de2d5075b576f441cf73368b2f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-11 20:06:39 +0700

    erlang compiler: const folding and tail recursion

--- tests/machine/x/erlang/tail_recursion.erl
+++ tests/machine/x/erlang/tail_recursion.erl
@@ -2,8 +2,7 @@
 % tail_recursion.erl - generated from tail_recursion.mochi
 
 sum_rec(N, Acc) ->
-    (if (N == 0) -> Acc; true -> ok end),
-    sum_rec((N - 1), (Acc + N)).
+    case (N == 0) of true -> Acc; _ -> sum_rec((N - 1), (Acc + N)) end.
 
 main(_) ->
     io:format("~p~n", [sum_rec(10, 0)]).
