2acd9c7cdaf5d358f6086399bdac3e1e9411175f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-11 12:48:50 +0700

    scala compiler: improve map detection and query sort

--- tests/machine/x/scala/group_by_conditional_sum.scala
+++ tests/machine/x/scala/group_by_conditional_sum.scala
@@ -16,7 +16,7 @@ object group_by_conditional_sum {
   }
 
   val items = List[Item](Item(cat = "a", `val` = 10, flag = true), Item(cat = "a", `val` = 5, flag = false), Item(cat = "b", `val` = 20, flag = true))
-  val result = (((for { i <- items } yield (i.cat, (i))).groupBy(_._1).map{ case(k,list) => (k, list.map(_._2)) }.toList).sortBy(g => g.key)).map{ case(gKey,gItems) => { val g = (gKey, gItems); Result(cat = g._1, share = (for { x <- g._2 } yield if (_truthy(x.flag)) x.`val` else 0).sum / (for { x <- g._2 } yield x.`val`).sum) } }.toList
+  val result = (((for { i <- items } yield (i.cat, (i))).groupBy(_._1).map{ case(k,list) => (k, list.map(_._2)) }.toList).sortBy(g => g._1)).map{ case(gKey,gItems) => { val g = (gKey, gItems); Result(cat = g._1, share = (for { x <- g._2 } yield if (_truthy(x.flag)) x.`val` else 0).sum / (for { x <- g._2 } yield x.`val`).sum) } }.toList
   def main(args: Array[String]): Unit = {
     println((result))
   }
--- tests/machine/x/scala/map_nested_assign.scala
+++ tests/machine/x/scala/map_nested_assign.scala
@@ -1,8 +1,6 @@
 object map_nested_assign {
-  case class Data(inner: Int)
-
   def main(args: Array[String]): Unit = {
-    var data = scala.collection.mutable.Map[String, Map[String, Int]]("outer" -> (Data(inner = 1)))
+    var data = scala.collection.mutable.Map[String, Map[String, Int]]("outer" -> (Map[String, Int]("inner" -> (1))))
     val _tmp0 = data("outer").updated("inner", 2)
     data = data.updated("outer", _tmp0)
     println((data("outer")("inner")))
