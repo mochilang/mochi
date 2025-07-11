131adc0e1b0f8697392bb3a4d8088861b6fb94fc
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-11 15:17:55 +0700

    zig: constant str builtin

--- tests/machine/x/zig/str_builtin.zig
+++ tests/machine/x/zig/str_builtin.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("{s}\n", .{std.fmt.allocPrint(std.heap.page_allocator, "{d}", .{123}) catch unreachable});
+    std.debug.print("{s}\n", .{"123"});
 }
