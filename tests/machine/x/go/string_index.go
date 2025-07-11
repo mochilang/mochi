b3f96745fcaf0bf2067118aa6ef7ca4eca9856cb
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-11 15:50:58 +0700

    go compiler: use built-in string indexing

--- tests/machine/x/go/string_index.go
+++ tests/machine/x/go/string_index.go
@@ -8,16 +8,5 @@ import (
 
 func main() {
 	var s string = "mochi"
-	fmt.Println(_indexString(s, 1))
-}
-
-func _indexString(s string, i int) string {
-	runes := []rune(s)
-	if i < 0 {
-		i += len(runes)
-	}
-	if i < 0 || i >= len(runes) {
-		panic("index out of range")
-	}
-	return string(runes[i])
+	fmt.Println(string([]rune(s)[1]))
 }
