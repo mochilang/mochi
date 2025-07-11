0d592be95d58850a50cc4ece5df65c74db1917cb
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-11 12:00:48 +0700

    cpp compiler: improve type inference

--- tests/machine/x/cpp/math_ops.cpp
+++ tests/machine/x/cpp/math_ops.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-  std::cout << std::boolalpha << (6 * 7) << std::endl;
-  std::cout << std::boolalpha << (7 / 2) << std::endl;
-  std::cout << std::boolalpha << (7 % 2) << std::endl;
+  std::cout << (6 * 7) << std::endl;
+  std::cout << (7 / 2) << std::endl;
+  std::cout << (7 % 2) << std::endl;
   return 0;
 }
