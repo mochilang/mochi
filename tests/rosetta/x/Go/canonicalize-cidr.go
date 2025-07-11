9e6c736f90c7627101196cee61af6e446026507d
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-12 01:24:28 +0700

    Add Canonicalize CIDR task

--- tests/rosetta/x/Go/canonicalize-cidr.go
+++ tests/rosetta/x/Go/canonicalize-cidr.go
@@ -16,15 +16,11 @@ func check(err error) {
 	}
 }
 
-// canonicalize a CIDR block: make sure none of the host bits are set
 func canonicalize(cidr string) string {
-	// dotted-decimal / bits in network part
 	split := strings.Split(cidr, "/")
 	dotted := split[0]
 	size, err := strconv.Atoi(split[1])
 	check(err)
-
-	// get IP as binary string
 	var bin []string
 	for _, n := range strings.Split(dotted, ".") {
 		i, err := strconv.Atoi(n)
@@ -32,19 +28,13 @@ func canonicalize(cidr string) string {
 		bin = append(bin, fmt.Sprintf("%08b", i))
 	}
 	binary := strings.Join(bin, "")
-
-	// replace the host part with all zeros
-	binary = binary[0:size] + strings.Repeat("0", 32-size)
-
-	// convert back to dotted-decimal
+	binary = binary[:size] + strings.Repeat("0", 32-size)
 	var canon []string
 	for i := 0; i < len(binary); i += 8 {
 		num, err := strconv.ParseInt(binary[i:i+8], 2, 64)
 		check(err)
 		canon = append(canon, fmt.Sprintf("%d", num))
 	}
-
-	// and return
 	return strings.Join(canon, ".") + "/" + split[1]
 }
 
@@ -57,7 +47,6 @@ func main() {
 		"161.214.74.21/24",
 		"184.232.176.184/18",
 	}
-
 	for _, test := range tests {
 		fmt.Printf("%-18s -> %s\n", test, canonicalize(test))
 	}
