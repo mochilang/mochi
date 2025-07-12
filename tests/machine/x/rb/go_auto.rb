c95e551114ec2f376c6368815b00b9b95be7b3d8
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-12 23:17:10 +0700

    ruby compiler: support basic FFI imports

--- tests/machine/x/rb/go_auto.rb
+++ tests/machine/x/rb/go_auto.rb
@@ -1,3 +1,5 @@
+testpkg = Struct.new(:Add, :Pi, :Answer).new(->(a,b){ a + b }, 3.14, 42)
+
 puts(testpkg.Add.call(2, 3))
 puts(testpkg.Pi)
 puts(testpkg.Answer)
--- tests/machine/x/rb/python_auto.rb
+++ tests/machine/x/rb/python_auto.rb
@@ -1,2 +1,4 @@
+math = Struct.new(:pi, :e, :sqrt, :pow, :sin, :log).new(Math::PI, Math::E, Math.method(:sqrt), ->(x,y){ x**y }, Math.method(:sin), Math.method(:log))
+
 puts(math.sqrt.call(16.0))
 puts(math.pi)
