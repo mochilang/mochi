b3ce5a4249b29e32162e5ce4ad0512a5285f63e9
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-12 20:36:27 +0700

    fix scala compiler group by handling and update machine outputs

--- tests/machine/x/scala/group_by_multi_join.scala
+++ tests/machine/x/scala/group_by_multi_join.scala
@@ -1,7 +1,6 @@
 object group_by_multi_join {
   case class Filtered(part: Int, value: Double)
-  case class Grouped(x: Filtered)
-  case class Grouped1(part: Int, total: Int)
+  case class Grouped(part: Int, total: Int)
   case class Nation(id: Int, name: String)
   case class Partsupp(part: Int, supplier: Int, cost: Double, qty: Int)
   case class Supplier(id: Int, nation: Int)
@@ -12,7 +11,7 @@ object group_by_multi_join {
   val suppliers = List[Supplier](Supplier(id = 1, nation = 1), Supplier(id = 2, nation = 2))
   val partsupp = List[Partsupp](Partsupp(part = 100, supplier = 1, cost = 10, qty = 2), Partsupp(part = 100, supplier = 2, cost = 20, qty = 1), Partsupp(part = 200, supplier = 1, cost = 5, qty = 3))
   val filtered = for { ps <- partsupp; s <- suppliers; if (s.id).asInstanceOf[Int] == ps.supplier; n <- nations; if (n.id).asInstanceOf[Int] == s.nation; if n.name == "A" } yield Filtered(part = ps.part, value = ps.cost * ps.qty)
-  val grouped = ((for { x <- filtered } yield (x.part, Grouped(x = x))).groupBy(_._1).map{ case(k,list) => _Group(k, list.map(_._2)) }.toList).map{ g => Grouped1(part = g.key, total = (for { r <- g } yield r.value).sum) }.toList
+  val grouped = ((for { x <- filtered } yield (x.part, x)).groupBy(_._1).map{ case(k,list) => _Group(k, list.map(_._2)) }.toList).map{ g => Grouped(part = g.key, total = (for { r <- g } yield r.value).sum) }.toList
   def main(args: Array[String]): Unit = {
     println((grouped))
   }
--- tests/machine/x/scala/group_by_sort.scala
+++ tests/machine/x/scala/group_by_sort.scala
@@ -1,12 +1,11 @@
 object group_by_sort {
-  case class Grouped(i: Item)
-  case class Grouped1(cat: String, total: Int)
+  case class Grouped(cat: String, total: Int)
   case class Item(cat: String, `val`: Int)
 
   case class _Group[K,T](key: K, items: List[T]) extends Iterable[T] { def iterator: Iterator[T] = items.iterator }
 
   val items = List[Item](Item(cat = "a", `val` = 3), Item(cat = "a", `val` = 1), Item(cat = "b", `val` = 5), Item(cat = "b", `val` = 2))
-  val grouped = (((for { i <- items } yield (i.cat, Grouped(i = i))).groupBy(_._1).map{ case(k,list) => _Group(k, list.map(_._2)) }.toList).sortBy(g => -(for { x <- g } yield x.`val`).sum)).map{ g => Grouped1(cat = g.key, total = (for { x <- g } yield x.`val`).sum) }.toList
+  val grouped = (((for { i <- items } yield (i.cat, i)).groupBy(_._1).map{ case(k,list) => _Group(k, list.map(_._2)) }.toList).sortBy(g => -(for { x <- g } yield x.`val`).sum)).map{ g => Grouped(cat = g.key, total = (for { x <- g } yield x.`val`).sum) }.toList
   def main(args: Array[String]): Unit = {
     println((grouped))
   }
