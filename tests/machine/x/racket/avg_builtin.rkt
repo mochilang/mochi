0bb6533878ff447c8c9122b234b10d94ff36486a
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-12 23:20:50 +0700

    racket: improve avg builtin and regenerate tests

--- tests/machine/x/racket/avg_builtin.rkt
+++ tests/machine/x/racket/avg_builtin.rkt
@@ -1,2 +1,2 @@
 #lang racket
-(displayln (let ([xs '(1 2 3)] [n (length '(1 2 3))]) (if (= n 0) 0 (/ (for/fold ([s 0]) ([v xs]) (+ s v)) n))))
+(displayln (let ([xs '(1 2 3)] [n (length '(1 2 3))]) (if (= n 0) 0 (/ (apply + xs) n))))
--- tests/machine/x/racket/load_yaml.rkt
+++ tests/machine/x/racket/load_yaml.rkt
@@ -3,7 +3,7 @@
 (require file/paths)
 (struct Person (name age email) #:transparent #:mutable)
 (define people (yaml-load (file->string (build-path (path-only (current-load-relative-directory)) ".." "interpreter" "valid" "people.yaml"))))
-(define adults (for*/list ([p people] #:when (and (cond [(string? (hash-ref p 'age)) (string>=? (hash-ref p 'age) 18)] [(string? 18) (string>=? (hash-ref p 'age) 18)] [else (>= (hash-ref p 'age) 18)]))) (hash 'name (hash-ref p 'name) 'email (hash-ref p 'email))))
+(define adults (for*/list ([p people] #:when (and (>= (hash-ref p 'age) 18))) (hash 'name (hash-ref p 'name) 'email (hash-ref p 'email))))
 (for ([a (if (hash? adults) (hash-keys adults) adults)])
 (displayln (string-join (map ~a (list (hash-ref a 'name) (hash-ref a 'email))) " "))
 )
