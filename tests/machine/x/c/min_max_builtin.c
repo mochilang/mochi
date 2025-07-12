34afd967b82d18718e882cb006b4ef124d8357ef
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-12 11:52:00 +0700

    c backend: avoid list_int for basic aggregates

--- tests/machine/x/c/min_max_builtin.c
+++ tests/machine/x/c/min_max_builtin.c
@@ -1,43 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
-  int len;
-  int *data;
-} list_int;
-static list_int list_int_create(int len) {
-  list_int l;
-  l.len = len;
-  l.data = calloc(len, sizeof(int));
-  if (!l.data && len > 0) {
-    fprintf(stderr, "alloc failed\n");
-    exit(1);
-  }
-  return l;
-}
-static int _min_int(list_int v) {
-  if (v.len == 0)
-    return 0;
-  int m = v.data[0];
-  for (int i = 1; i < v.len; i++)
-    if (v.data[i] < m)
-      m = v.data[i];
-  return m;
-}
-static int _max_int(list_int v) {
-  if (v.len == 0)
-    return 0;
-  int m = v.data[0];
-  for (int i = 1; i < v.len; i++)
-    if (v.data[i] > m)
-      m = v.data[i];
-  return m;
-}
 int main() {
-  int tmp1_data[] = {3, 1, 4};
-  list_int tmp1 = {3, tmp1_data};
-  list_int nums = tmp1;
-  printf("%d\n", _min_int(nums));
-  printf("%d\n", _max_int(nums));
+  int nums[] = {3, 1, 4};
+  int tmp1[] = {3, 1, 4};
+  int tmp2 = tmp1[0];
+  for (int i3 = 1; i3 < 3; i3++) {
+    if (tmp1[i3] < tmp2)
+      tmp2 = tmp1[i3];
+  }
+  printf("%d\n", tmp2);
+  int tmp4[] = {3, 1, 4};
+  int tmp5 = tmp4[0];
+  for (int i6 = 1; i6 < 3; i6++) {
+    if (tmp4[i6] > tmp5)
+      tmp5 = tmp4[i6];
+  }
+  printf("%d\n", tmp5);
   return 0;
 }
