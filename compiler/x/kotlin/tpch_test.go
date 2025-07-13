9988cb46d3183424d7cb2a0a7d9c3a6f834cbe9a
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-14 00:07:09 +0700

    kotlin: add tpch q1 test

--- compiler/x/kotlin/tpch_test.go
+++ compiler/x/kotlin/tpch_test.go
@@ -58,7 +58,7 @@ func TestKotlinCompiler_TPCH(t *testing.T) {
 	if _, err := exec.LookPath("kotlinc"); err != nil {
 		t.Skip("kotlinc not installed")
 	}
-	for _, base := range []string{"q11"} {
+	for _, base := range []string{"q1", "q11"} {
 		t.Run(base, func(t *testing.T) { runTPCHQuery(t, base) })
 	}
 }
