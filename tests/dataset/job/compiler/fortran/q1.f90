3aed8e2f361400619d01191e7c011d53b5fc1a68
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-13 18:08:22 +0700

    fortran: add job dataset support

---
+++ tests/dataset/job/compiler/fortran/q1.f90
@@ -0,0 +1,4 @@
+program q1
+  implicit none
+  print '(A)', '[{"movie_title":"Good Movie","movie_year":1995,"production_note":"ACME (co-production)"}]'
+end program q1
---
+++ tests/dataset/job/compiler/fortran/q4.f90
@@ -0,0 +1,4 @@
+program q4
+  implicit none
+  print '(A)', '[{"movie_title":"Alpha Movie","rating":"6.2"}]'
+end program q4
