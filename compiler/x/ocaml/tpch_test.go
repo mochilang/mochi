87bdffeaf6c283be08eacfc04d7242043ca34b95
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-13 23:22:26 +0700

    ocaml: compile tpch q6-q10

--- compiler/x/ocaml/tpch_test.go
+++ compiler/x/ocaml/tpch_test.go
@@ -17,7 +17,7 @@ func TestOCamlCompiler_TPCH(t *testing.T) {
 	if _, err := exec.LookPath("ocamlc"); err != nil {
 		t.Skipf("ocamlc not installed: %v", err)
 	}
-	for i := 1; i <= 5; i++ {
+	for i := 1; i <= 10; i++ {
 		q := fmt.Sprintf("q%d", i)
 		testutil.CompileTPCH(t, q, func(env *types.Env, prog *parser.Program) ([]byte, error) {
 			return ocaml.New(env).Compile(prog, "")
