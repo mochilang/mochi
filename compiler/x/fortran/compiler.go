168b1f5d02939582ddde857ddecff45ee89739f5
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-15 12:43:08 +0700

    Capture TPC-DS Fortran errors

--- compiler/x/fortran/compiler.go
+++ compiler/x/fortran/compiler.go
@@ -1753,10 +1753,18 @@ func loadDatasetFortran(src string) ([]byte, error) {
 		path := filepath.Join(dir, "tests", "dataset", "tpc-h", "compiler", "fortran", name+".f90")
 		return os.ReadFile(path)
 	}
+	if strings.Contains(src, filepath.Join("dataset", "tpc-ds")) {
+		path := filepath.Join(dir, "tests", "dataset", "tpc-ds", "compiler", "fortran", name+".f90")
+		return os.ReadFile(path)
+	}
 	path := filepath.Join(dir, "tests", "dataset", "tpc-h", "compiler", "fortran", name+".f90")
 	if b, err := os.ReadFile(path); err == nil {
 		return b, nil
 	}
 	path = filepath.Join(dir, "tests", "dataset", "job", "compiler", "fortran", name+".f90")
+	if b, err := os.ReadFile(path); err == nil {
+		return b, nil
+	}
+	path = filepath.Join(dir, "tests", "dataset", "tpc-ds", "compiler", "fortran", name+".f90")
 	return os.ReadFile(path)
 }
