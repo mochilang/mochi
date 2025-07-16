bf64094cd1e929412cc294ce4fcff0ca141e3a44
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-16 19:09:19 +0700

    run Scala rosetta golden tests

--- types/check.go
+++ types/check.go
@@ -2689,10 +2689,14 @@ func checkBuiltinCall(name string, args []Type, pos lexer.Position) error {
 			}
 		}
 		for i := 1; i < 3; i++ {
-			if _, ok := args[i].(IntType); !ok {
-				if _, ok := args[i].(AnyType); !ok {
-					return errArgTypeMismatch(pos, i, IntType{}, args[i])
-				}
+			switch args[i].(type) {
+			case IntType, AnyType:
+				// ok
+			case BigIntType:
+				// allow bigint indices, treat as int
+				args[i] = IntType{}
+			default:
+				return errArgTypeMismatch(pos, i, IntType{}, args[i])
 			}
 		}
 		return nil
