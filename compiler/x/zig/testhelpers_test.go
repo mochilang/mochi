e443bd76004f259e1f8c1307058c480a964fb15d
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-17 00:08:20 +0700

    zig: avoid flag conflict

--- compiler/x/zig/testhelpers_test.go
+++ compiler/x/zig/testhelpers_test.go
@@ -7,8 +7,6 @@ import (
 	"flag"
 )
 
-var update = flag.Bool("update", false, "update golden files")
-
 func shouldUpdate() bool {
 	f := flag.Lookup("update")
 	return f != nil && f.Value.String() == "true"
