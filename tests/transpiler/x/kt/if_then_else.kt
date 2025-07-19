023b5d3c7be694a9bf6e212c02f5fe217bc57874
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-19 19:40:45 +0700

    kt tasks: record progress

---
+++ tests/transpiler/x/kt/if_then_else.kt
@@ -0,0 +1,6 @@
+// Mochi 0.10.31 - generated 2025-07-19 12:28:27 UTC
+fun main() {
+    val x = 12
+    val msg = if ((x > 10)) "yes" else "no"
+    println(msg)
+}
---
+++ tests/transpiler/x/kt/if_then_else_nested.kt
@@ -0,0 +1,6 @@
+// Mochi 0.10.31 - generated 2025-07-19 12:28:27 UTC
+fun main() {
+    val x = 8
+    val msg = if ((x > 10)) "big" else if ((x > 5)) "medium" else "small"
+    println(msg)
+}
