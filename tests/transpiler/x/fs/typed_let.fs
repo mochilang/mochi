6f243aa1786818df070877611ce8dbd7dbb210be
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-19 14:15:38 +0700

    fs transpiler: add mutable vars and loops

---
+++ tests/transpiler/x/fs/typed_let.fs
@@ -0,0 +1,5 @@
+// Mochi 0.10.31 - generated 2025-07-19 06:51:09 UTC
+open System
+
+let y: int = 0
+printfn "%O" y
---
+++ tests/transpiler/x/fs/typed_var.fs
@@ -0,0 +1,5 @@
+// Mochi 0.10.31 - generated 2025-07-19 06:51:09 UTC
+open System
+
+let mutable x: int = 0
+printfn "%O" x
