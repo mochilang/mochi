5f112a82da08f49fb429fc722ac400b8c9b4b134
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-19 13:43:57 +0700

    fs tasks: record latest progress

---
+++ tests/transpiler/x/fs/append_builtin.fs
@@ -0,0 +1,5 @@
+// Mochi 0.10.31 - generated 2025-07-19 06:29:36 UTC
+open System
+
+let a = [1; 2]
+printfn "%O" (a @ [3])
---
+++ tests/transpiler/x/fs/len_string.fs
@@ -0,0 +1,4 @@
+// Mochi 0.10.31 - generated 2025-07-19 06:29:36 UTC
+open System
+
+printfn "%O" Seq.length "mochi"
