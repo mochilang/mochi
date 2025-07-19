9c9778f1857be74b1618f405ccfc284a2054d720
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-19 09:07:45 +0700

    rust: return i32 from now builtin

--- types/infer.go
+++ types/infer.go
@@ -470,7 +470,7 @@ func inferPrimaryType(env *Env, p *parser.Primary) Type {
 			}
 			return ListType{Elem: AnyType{}}
 		case "now":
-			return Int64Type{}
+			return IntType{}
 		case "to_json":
 			return StringType{}
 		default:
