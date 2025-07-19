6f004705882de4534c57d47ef1588bb7ba18b8bb
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-19 12:40:43 +0700

    Move Kotlin docs and extend transpiler

---
+++ tests/transpiler/x/kt/len_builtin.kt
@@ -0,0 +1,4 @@
+// Mochi 0.10.31 - generated 2025-07-19 05:27:40 UTC
+fun main() {
+    println(listOf(1, 2, 3).size)
+}
---
+++ tests/transpiler/x/kt/str_builtin.kt
@@ -0,0 +1,4 @@
+// Mochi 0.10.31 - generated 2025-07-19 05:27:47 UTC
+fun main() {
+    println(123.toString())
+}
