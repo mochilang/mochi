8b43029f19f796d1070e1b61774b33ec1d773676
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-21 15:40:22 +0700

    java transpiler: improve membership operator

--- tests/transpiler/x/java/in_operator.java
+++ tests/transpiler/x/java/in_operator.java
@@ -2,7 +2,7 @@ public class Main {
     static int[] xs = new int[]{1, 2, 3};
 
     public static void main(String[] args) {
-        System.out.println(java.util.Arrays.asList(xs).contains(2));
-        System.out.println(!(java.util.Arrays.asList(xs).contains(5)));
+        System.out.println(java.util.Arrays.stream(xs).anyMatch((int x) -> x == 2));
+        System.out.println(!(java.util.Arrays.stream(xs).anyMatch((int x) -> x == 5)));
     }
 }
