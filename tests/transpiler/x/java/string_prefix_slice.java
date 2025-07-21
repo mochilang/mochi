866e6971dc1a10827911e56e8a05053ea08ab1e8
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-21 07:34:44 +0700

    java transpiler: support array slices and numeric bools

--- tests/transpiler/x/java/string_prefix_slice.java
+++ tests/transpiler/x/java/string_prefix_slice.java
@@ -4,7 +4,7 @@ public class Main {
     static String s2 = "desert";
 
     public static void main(String[] args) {
-        System.out.println((s1.substring(0, prefix.length()).equals(prefix)));
-        System.out.println((s2.substring(0, prefix.length()).equals(prefix)));
+        System.out.println((s1.substring(0, prefix.length()).equals(prefix)) ? 1 : 0);
+        System.out.println((s2.substring(0, prefix.length()).equals(prefix)) ? 1 : 0);
     }
 }
