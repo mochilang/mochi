9df5c5fd7ad8a0b3b0cbed1390926fba761a6bf3
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-21 07:42:46 +0700

    rb transpiler: improve type inference

--- transpiler/x/rb/transpiler_test.go
+++ transpiler/x/rb/transpiler_test.go
@@ -106,14 +106,15 @@ func TestTranspilePrograms(t *testing.T) {
 		"closure",
 		"json_builtin",
 		"tree_sum",
-               "test_block",
-               "list_set_ops",
-               "cross_join_triple",
-               "dataset_where_filter",
-               "inner_join",
-               "join_multi",
-               "in_operator_extended",
-       }
+		"test_block",
+		"list_set_ops",
+		"cross_join_triple",
+		"exists_builtin",
+		"dataset_where_filter",
+		"inner_join",
+		"join_multi",
+		"in_operator_extended",
+	}
 	root := repoRoot(t)
 	outDir := filepath.Join(root, "tests", "transpiler", "x", "rb")
 	os.MkdirAll(outDir, 0o755)
