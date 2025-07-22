42d9e39234a7d99b3ce63dc0179f93f6f4d98fcd
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-22 09:38:47 +0700

    java transpiler: capitalize booleans

--- tests/transpiler/x/java/cross_join_triple.java
+++ tests/transpiler/x/java/cross_join_triple.java
@@ -2,7 +2,7 @@ public class Main {
     static int[] nums = new int[]{1, 2};
     static String[] letters = new String[]{"A", "B"};
     static boolean[] bools = new boolean[]{true, false};
-    static java.util.List<Result2> combos = new java.util.ArrayList<Result2>() {{ java.util.ArrayList<Result2> _tmp = new java.util.ArrayList<>(); for (var n : nums) { for (var l : letters) { for (var b : bools) { _tmp.add(new Result2(n, l, b)); } } } java.util.ArrayList<Result2> list = _tmp; int skip = 0; int take = -1; for (int i = 0; i < list.size(); i++) { if (i < skip) continue; if (take >= 0 && i >= skip + take) break; _tmp.add((Result2)list.get(i)); } addAll(_tmp);}};
+    static java.util.List<Result2> combos = new java.util.ArrayList<Result2>() {{ java.util.ArrayList<Result2> _tmp = new java.util.ArrayList<>(); for (var n : nums) { for (var l : letters) { for (var b : bools) { _tmp.add(new Result2(n, l, b)); } } } java.util.ArrayList<Result2> list = _tmp; java.util.ArrayList<Result2> _res = new java.util.ArrayList<>(); int skip = 0; int take = -1; for (int i = 0; i < list.size(); i++) { if (i < skip) continue; if (take >= 0 && i >= skip + take) break; _res.add((Result2)list.get(i)); } addAll(_res);}};
     static class Result2 {
         int n;
         String l;
@@ -12,19 +12,26 @@ public class Main {
             this.l = l;
             this.b = b;
         }
-        boolean containsKey(String k) {
-            if (k.equals("n")) return true;
-            if (k.equals("l")) return true;
-            if (k.equals("b")) return true;
-            return false;
+        @Override public String toString() {
+            return String.format("{'n': %s, 'l': '%s', 'b': %s}", q(n), q(l), q(b));
         }
     }
 
 
+    static String q(Object v) {
+        if (v instanceof String) return "'" + v.toString() + "'";
+        return String.valueOf(v);
+    }
+
+    static String boolStr(Object v) {
+        if (v instanceof Boolean b) return b ? "True" : "False";
+        return String.valueOf(v);
+    }
+
     public static void main(String[] args) {
-        System.out.println("--- Cross Join of three lists ---");
+        System.out.println(boolStr("--- Cross Join of three lists ---"));
         for (var c : combos) {
-            System.out.println(c.n + " " + c.l + " " + c.b);
+            System.out.println(boolStr(c.n) + " " + boolStr(c.l) + " " + boolStr(c.b));
         }
     }
 }
