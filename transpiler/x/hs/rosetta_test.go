dc1ec9f092a82447e1f9326fd5086c80a2e12162
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-24 14:00:04 +0700

    Update Haskell transpiler and tests

--- transpiler/x/hs/rosetta_test.go
+++ transpiler/x/hs/rosetta_test.go
@@ -81,6 +81,7 @@ func runRosettaTask(root, name string) error {
 	}
 
 	cmd := exec.Command("runhaskell", hsFile)
+	cmd.Env = append(os.Environ(), "MOCHI_NOW_SEED=1")
 	if data, err := os.ReadFile(strings.TrimSuffix(src, ".mochi") + ".in"); err == nil {
 		cmd.Stdin = bytes.NewReader(data)
 	}
