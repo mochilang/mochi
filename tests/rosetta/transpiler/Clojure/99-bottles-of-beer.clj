d4e6fce90cb374bb68f72a597dcafa963c23a295
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-25 21:26:18 +0700

    transpiler/clj: generate outputs for tests 14 and 15

--- tests/rosetta/transpiler/Clojure/99-bottles-of-beer.clj
+++ tests/rosetta/transpiler/Clojure/99-bottles-of-beer.clj
@@ -2,12 +2,15 @@
 
 (require 'clojure.set)
 
+(defn in [x coll]
+  (cond (string? coll) (clojure.string/includes? coll x) (map? coll) (contains? coll x) (sequential? coll) (some (fn [e] (= e x)) coll) :else false))
+
 (def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
 
 (declare bottles main)
 
 (defn bottles [n]
-  (do (when (= n 0) (throw (ex-info "return" {:v "No more bottles"}))) (if (= n 1) "1 bottle" (str (str n) " bottles"))))
+  (try (do (when (= n 0) (throw (ex-info "return" {:v "No more bottles"}))) (if (= n 1) "1 bottle" (str (str n) " bottles"))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
 
 (defn main []
   (do (def i 99) (while (> i 0) (do (println (str (bottles i) " of beer on the wall")) (println (str (bottles i) " of beer")) (println "Take one down, pass it around") (println (str (bottles (- i 1)) " of beer on the wall")) (def i (- i 1))))))
