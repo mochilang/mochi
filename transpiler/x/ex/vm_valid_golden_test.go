c089e46ff22438a0f3d833d73352847fb5c14d0a
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-25 07:53:00 +0700

    ex transpiler: add bench block

--- transpiler/x/ex/vm_valid_golden_test.go
+++ transpiler/x/ex/vm_valid_golden_test.go
@@ -57,6 +57,7 @@ func TestExTranspiler_VMValid_Golden(t *testing.T) {
 			return nil, err
 		}
 		cmd := exec.Command("elixir", codePath)
+		cmd.Env = append(os.Environ(), "MOCHI_NOW_SEED=1")
 		if data, err := os.ReadFile(strings.TrimSuffix(src, ".mochi") + ".in"); err == nil {
 			cmd.Stdin = bytes.NewReader(data)
 		}
