2bedd8669b49dee0e972b23cf052dfb3bd924b7b
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-26 19:50:16 +0700

    kotlin: add rosetta task 72 arithmetic integer 2

--- tests/rosetta/transpiler/Kotlin/arithmetic-evaluation.kt
+++ tests/rosetta/transpiler/Kotlin/arithmetic-evaluation.kt
@@ -1,8 +1,34 @@
+var _nowSeed = 0L
+var _nowSeeded = false
+fun _now(): Long {
+    if (!_nowSeeded) {
+        System.getenv("MOCHI_NOW_SEED")?.toLongOrNull()?.let {
+            _nowSeed = it
+            _nowSeeded = true
+        }
+    }
+    return if (_nowSeeded) {
+        _nowSeed = (_nowSeed * 1664525 + 1013904223) % 2147483647
+        kotlin.math.abs(_nowSeed)
+    } else {
+        kotlin.math.abs(System.nanoTime())
+    }
+}
+
+fun toJson(v: Any?): String = when (v) {
+    null -> "null"
+    is String -> "\"" + v.replace("\"", "\\\"") + "\""
+    is Boolean, is Number -> v.toString()
+    is Map<*, *> -> v.entries.joinToString(prefix = "{", postfix = "}") { toJson(it.key.toString()) + ":" + toJson(it.value) }
+    is Iterable<*> -> v.joinToString(prefix = "[", postfix = "]") { toJson(it) }
+    else -> toJson(v.toString())
+}
+
 data class Parser(var expr: String, var pos: Int)
 data class Res(var v: Int, var p: Parser)
 fun skipWS(p: Parser): Parser {
     var i: Int = p.pos
-    while ((i < p.expr.length) && (p.expr.substring(i, i + 1) == " ")) {
+    while ((i < (p.expr).length) && (p.expr.substring(i, i + 1) == " ")) {
         i = i + 1
     }
     p.pos = i
@@ -23,7 +49,7 @@ fun parseNumber(p: Parser): Res {
     var p: Parser = p
     p = skipWS(p)
     var start: Int = p.pos
-    while (p.pos < p.expr.length) {
+    while (p.pos < (p.expr).length) {
         val ch: String = p.expr.substring(p.pos, p.pos + 1)
         if ((ch >= "0") && (ch <= "9")) {
             p.pos = p.pos + 1
@@ -38,23 +64,23 @@ fun parseNumber(p: Parser): Res {
 fun parseFactor(p: Parser): Res {
     var p: Parser = p
     p = skipWS(p)
-    if ((p.pos < p.expr.length) && (p.expr.substring(p.pos, p.pos + 1) == "(")) {
+    if ((p.pos < (p.expr).length) && (p.expr.substring(p.pos, p.pos + 1) == "(")) {
         p.pos = p.pos + 1
         var r: Res = parseExpr(p)
-        var v = r.v
-        p = r.p as Parser
+        var v: Int = r.v
+        p = r.p
         p = skipWS(p)
-        if ((p.pos < p.expr.length) && (p.expr.substring(p.pos, p.pos + 1) == ")")) {
+        if ((p.pos < (p.expr).length) && (p.expr.substring(p.pos, p.pos + 1) == ")")) {
             p.pos = p.pos + 1
         }
         return Res(v = v, p = p)
     }
-    if ((p.pos < p.expr.length) && (p.expr.substring(p.pos, p.pos + 1) == "-")) {
+    if ((p.pos < (p.expr).length) && (p.expr.substring(p.pos, p.pos + 1) == "-")) {
         p.pos = p.pos + 1
         var r: Res = parseFactor(p)
-        var v = r.v
-        p = r.p as Parser
-        return Res(v = 0 - (v as Int), p = p)
+        var v: Int = r.v
+        p = r.p
+        return Res(v = 0 - v, p = p)
     }
     return parseNumber(p)
 }
@@ -76,16 +102,16 @@ fun powInt(base: Int, exp: Int): Int {
 fun parsePower(p: Parser): Res {
     var p: Parser = p
     var r: Res = parseFactor(p)
-    var v = r.v
-    p = r.p as Parser
+    var v: Int = r.v
+    p = r.p
     while (true) {
         p = skipWS(p)
-        if ((p.pos < p.expr.length) && (p.expr.substring(p.pos, p.pos + 1) == "^")) {
+        if ((p.pos < (p.expr).length) && (p.expr.substring(p.pos, p.pos + 1) == "^")) {
             p.pos = p.pos + 1
             var r2: Res = parseFactor(p)
-            var rhs = r2.v
-            p = r2.p as Parser
-            v = powInt(v as Int, rhs as Int)
+            var rhs: Int = r2.v
+            p = r2.p
+            v = powInt(v, rhs)
         } else {
             break
         }
@@ -96,26 +122,26 @@ fun parsePower(p: Parser): Res {
 fun parseTerm(p: Parser): Res {
     var p: Parser = p
     var r: Res = parsePower(p)
-    var v = r.v
-    p = r.p as Parser
+    var v: Int = r.v
+    p = r.p
     while (true) {
         p = skipWS(p)
-        if (p.pos < p.expr.length) {
+        if (p.pos < (p.expr).length) {
             val op: String = p.expr.substring(p.pos, p.pos + 1)
             if (op == "*") {
                 p.pos = p.pos + 1
                 var r2: Res = parsePower(p)
-                var rhs = r2.v
-                p = r2.p as Parser
-                v = (v as Number).toDouble() * (rhs as Number).toDouble()
+                var rhs: Int = r2.v
+                p = r2.p
+                v = v * rhs
                 continue
             }
             if (op == "/") {
                 p.pos = p.pos + 1
                 var r2: Res = parsePower(p)
-                var rhs = r2.v
-                p = r2.p as Parser
-                v = (v as Int) / rhs.toInt()
+                var rhs: Int = r2.v
+                p = r2.p
+                v = v / rhs.toInt()
                 continue
             }
         }
@@ -127,26 +153,26 @@ fun parseTerm(p: Parser): Res {
 fun parseExpr(p: Parser): Res {
     var p: Parser = p
     var r: Res = parseTerm(p)
-    var v = r.v
-    p = r.p as Parser
+    var v: Int = r.v
+    p = r.p
     while (true) {
         p = skipWS(p)
-        if (p.pos < p.expr.length) {
+        if (p.pos < (p.expr).length) {
             val op: String = p.expr.substring(p.pos, p.pos + 1)
             if (op == "+") {
                 p.pos = p.pos + 1
                 var r2: Res = parseTerm(p)
-                var rhs = r2.v
-                p = r2.p as Parser
-                v = (v as Number).toDouble() + (rhs as Number).toDouble()
+                var rhs: Int = r2.v
+                p = r2.p
+                v = v + rhs
                 continue
             }
             if (op == "-") {
                 p.pos = p.pos + 1
                 var r2: Res = parseTerm(p)
-                var rhs = r2.v
-                p = r2.p as Parser
-                v = (v as Number).toDouble() - (rhs as Number).toDouble()
+                var rhs: Int = r2.v
+                p = r2.p
+                v = v - rhs
                 continue
             }
         }
@@ -157,8 +183,8 @@ fun parseExpr(p: Parser): Res {
 
 fun evalExpr(expr: String): Int {
     var p: Parser = Parser(expr = expr, pos = 0)
-    val r: Res = parseExpr(p as Parser)
-    return r.v as Int
+    val r: Res = parseExpr(p)
+    return r.v
 }
 
 fun user_main(): Unit {
@@ -167,5 +193,17 @@ fun user_main(): Unit {
 }
 
 fun main() {
-    user_main()
+    run {
+        System.gc()
+        val _startMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
+        val _start = _now()
+        user_main()
+        System.gc()
+        val _end = _now()
+        val _endMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
+        val _durationUs = (_end - _start) / 1000
+        val _memDiff = kotlin.math.abs(_endMem - _startMem)
+        val _res = mapOf("duration_us" to _durationUs, "memory_bytes" to _memDiff, "name" to "main")
+        println(toJson(_res))
+    }
 }
--- tests/rosetta/transpiler/Kotlin/arithmetic-geometric-mean.kt
+++ tests/rosetta/transpiler/Kotlin/arithmetic-geometric-mean.kt
@@ -24,40 +24,38 @@ fun toJson(v: Any?): String = when (v) {
     else -> toJson(v.toString())
 }
 
-val PI: Double = 3.141592653589793
-val degreesIncr: Double = (0.1 * PI) / 180.0
-val turns: Double = 2.0
-val stop: Double = ((360.0 * turns) * 10.0) * degreesIncr
-val width: Double = 600.0
-val centre: Double = width / 2.0
-val a: Double = 1.0
-val b: Double = 20.0
-var theta: Double = 0.0
-var count: Int = 0
-fun sinApprox(x: Double): Double {
-    var term: Double = x
-    var sum: Double = x
-    var n: Int = 1
-    while (n <= 10) {
-        val denom: Double = ((2 * n) * ((2 * n) + 1)).toDouble()
-        term = (((0.0 - term) * x) * x) / denom
-        sum = sum + term
-        n = n + 1
+fun abs(x: Double): Double {
+    if (x < 0.0) {
+        return 0.0 - x
     }
-    return sum
+    return x
 }
 
-fun cosApprox(x: Double): Double {
-    var term: Double = 1.0
-    var sum: Double = 1.0
-    var n: Int = 1
-    while (n <= 10) {
-        val denom: Double = (((2 * n) - 1) * (2 * n)).toDouble()
-        term = (((0.0 - term) * x) * x) / denom
-        sum = sum + term
-        n = n + 1
+fun sqrtApprox(x: Double): Double {
+    var guess: Double = x
+    var i: Int = 0
+    while (i < 20) {
+        guess = (guess + (x / guess)) / 2.0
+        i = i + 1
     }
-    return sum
+    return guess
+}
+
+fun agm(a: Double, g: Double): Double {
+    var g: Double = g
+    var a: Double = a
+    val eps: Double = 0.00000000000001
+    while (kotlin.math.abs(a - g) > (kotlin.math.abs(a) * eps)) {
+        val newA: Double = (a + g) / 2.0
+        val newG: Double = sqrtApprox(a * g)
+        a = newA
+        g = newG
+    }
+    return a
+}
+
+fun user_main(): Unit {
+    println(agm(1.0, 1.0 / sqrtApprox(2.0)).toString())
 }
 
 fun main() {
@@ -65,16 +63,7 @@ fun main() {
         System.gc()
         val _startMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
         val _start = _now()
-        while (theta < stop) {
-            val r: Double = a + (b * theta)
-            val x: Double = r * cosApprox(theta)
-            val y: Double = r * sinApprox(theta)
-            if ((count % 100) == 0) {
-                println(((centre + x).toString() + ",") + (centre - y).toString())
-            }
-            theta = theta + degreesIncr
-            count = count + 1
-        }
+        user_main()
         System.gc()
         val _end = _now()
         val _endMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
