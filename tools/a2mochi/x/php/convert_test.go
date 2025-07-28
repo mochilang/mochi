78963d9405586cc05635d67f8ad76301b692606c
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-28 14:37:10 +0700

    Add PHP converter skeleton

---
+++ tools/a2mochi/x/php/convert_test.go
@@ -0,0 +1,12 @@
+package php_test
+
+import (
+	"flag"
+	"testing"
+)
+
+var update = flag.Bool("update", false, "update golden files")
+
+func TestConvert_Golden(t *testing.T) {
+	t.Skip("PHP converter not fully implemented")
+}
