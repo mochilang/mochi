a63e58cb39a60c8f5b5c78dace1d20b5479b2cf8
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-28 20:21:54 +0700

    a2mochi/c: support closure

--- tools/a2mochi/x/c/convert_test.go
+++ tools/a2mochi/x/c/convert_test.go
@@ -51,7 +51,6 @@ func TestConvert_Golden(t *testing.T) {
 
 	skip := map[string]bool{
 		"bench_block":              true,
-		"closure":                  true,
 		"cross_join":               true,
 		"cross_join_filter":        true,
 		"cross_join_triple":        true,
