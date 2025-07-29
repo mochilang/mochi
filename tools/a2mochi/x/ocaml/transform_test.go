b3e4f101d85bc4a55d4a0bcabca278fb80ddf4af
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-29 15:07:27 +0700

    a2mochi/ocaml: support additional examples

--- tools/a2mochi/x/ocaml/transform_test.go
+++ tools/a2mochi/x/ocaml/transform_test.go
@@ -101,14 +101,18 @@ func TestTransform_Golden(t *testing.T) {
 		"if_then_else":        true,
 		"if_then_else_nested": true,
 		"test_block":          true,
-               "list_index":          true,
-               "append_builtin":      true,
-               "bool_chain":          true,
-               "fun_call":            true,
-               "fun_expr_in_let":    true,
-               "fun_three_args":     true,
-               "math_ops":           true,
-       }
+		"list_index":          true,
+		"append_builtin":      true,
+		"bool_chain":          true,
+		"fun_call":            true,
+		"fun_expr_in_let":     true,
+		"fun_three_args":      true,
+		"math_ops":            true,
+		"len_map":             true,
+		"str_builtin":         true,
+		"min_max_builtin":     true,
+		"membership":          true,
+	}
 	outDir := filepath.Join(root, "tests", "a2mochi", "x", "ocaml")
 	os.MkdirAll(outDir, 0o755)
 
