0853da5ec9ed76cd2067b971e30c2802550c1a42
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-30 15:39:46 +0700

    haskell json-ast: optional positions

--- tools/json-ast/x/haskell/inspect.go
+++ tools/json-ast/x/haskell/inspect.go
@@ -14,14 +14,18 @@ type Program struct {
 
 // Inspect parses the provided Haskell source code using tree-sitter and
 // returns its Program representation.
-func Inspect(src string) (*Program, error) {
+func Inspect(src string, includePos ...bool) (*Program, error) {
 	parser := sitter.NewParser()
 	parser.SetLanguage(sitter.NewLanguage(tsHaskell.Language()))
 	tree, err := parser.ParseCtx(context.Background(), nil, []byte(src))
 	if err != nil {
 		return nil, err
 	}
-	root := convert(tree.RootNode(), []byte(src))
+	pos := false
+	if len(includePos) > 0 && includePos[0] {
+		pos = true
+	}
+	root := convert(tree.RootNode(), []byte(src), pos)
 	if root == nil {
 		root = &Node{}
 	}
