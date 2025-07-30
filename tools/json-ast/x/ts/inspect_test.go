9191a817d79a23d7584b792aaebc271ed93dc00f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-30 15:31:02 +0700

    ts json-ast: optional positions

--- tools/json-ast/x/ts/inspect_test.go
+++ tools/json-ast/x/ts/inspect_test.go
@@ -47,7 +47,7 @@ func TestInspect_Golden(t *testing.T) {
 		if err != nil {
 			t.Fatalf("read src: %v", err)
 		}
-		prog, err := ts.Inspect(string(data))
+		prog, err := ts.Inspect(string(data), false)
 		if err != nil {
 			t.Fatalf("inspect: %v", err)
 		}
