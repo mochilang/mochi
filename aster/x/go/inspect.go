658f90e725f8dd147688f69d0523284b1932f7a3
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-30 17:54:03 +0700

    refactor go aster to use tree-sitter

--- aster/x/go/inspect.go
+++ aster/x/go/inspect.go
@@ -5,8 +5,8 @@ package gox
 import (
 	"context"
 
-	sitter "github.com/smacker/go-tree-sitter"
-	tsgolang "github.com/smacker/go-tree-sitter/golang"
+	sitter "github.com/tree-sitter/go-tree-sitter"
+	tsgolang "github.com/tree-sitter/tree-sitter-go/bindings/go"
 )
 
 // Program represents a parsed Go source file.
@@ -18,11 +18,8 @@ type Program struct {
 // representation.
 func Inspect(src string) (*Program, error) {
 	parser := sitter.NewParser()
-	parser.SetLanguage(tsgolang.GetLanguage())
-	tree, err := parser.ParseCtx(context.Background(), nil, []byte(src))
-	if err != nil {
-		return nil, err
-	}
+	parser.SetLanguage(sitter.NewLanguage(tsgolang.Language()))
+	tree := parser.ParseCtx(context.Background(), []byte(src), nil)
 	root := convertNode(tree.RootNode(), []byte(src))
 	return &Program{Root: root}, nil
 }
--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/tree-sitter/go-tree-sitter v0.25.0
 	github.com/tree-sitter/tree-sitter-c v0.23.4
 	github.com/tree-sitter/tree-sitter-fsharp v0.1.0
+	github.com/tree-sitter/tree-sitter-go v0.23.4
 	github.com/tree-sitter/tree-sitter-haskell v0.23.1
 	github.com/tree-sitter/tree-sitter-python v0.23.6
 	github.com/tree-sitter/tree-sitter-racket v0.24.7
