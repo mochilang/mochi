3c0231714129429d4bc93abbc715c8d3c1c34f6f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-31 21:04:21 +0700

    prolog: add more golden tests

---
+++ tests/transpiler/x/prolog/flatten_list.pl
@@ -0,0 +1,6 @@
+:- initialization(main).
+:- style_check(-singleton).
+
+main :-
+    flatten([1,[2,[3]],4], F),
+    writeln(F).
---
+++ tests/transpiler/x/prolog/string_length_builtin.pl
@@ -0,0 +1,6 @@
+:- initialization(main).
+:- style_check(-singleton).
+
+main :-
+    string_length("hello", L),
+    writeln(L).
