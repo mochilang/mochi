17968233cc9752d5b366c65034bb3e4dede67888
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-31 20:55:18 +0700

    haskell: add printer support for more programs

--- aster/x/haskell/print_test.go
+++ aster/x/haskell/print_test.go
@@ -31,8 +31,8 @@ func TestPrint_Golden(t *testing.T) {
 		t.Fatal(err)
 	}
 	sort.Strings(files)
-	if len(files) > 10 {
-		files = files[:10]
+	if len(files) > 25 {
+		files = files[:25]
 	}
 	// When not updating, only run tests for which golden files exist
 	var selected []string
