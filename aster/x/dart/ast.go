71de828ae04b430ebe11290ec3f9cfa1bb6e06af
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-31 19:02:02 +0700

    dart aster: extend examples and printer

--- aster/x/dart/ast.go
+++ aster/x/dart/ast.go
@@ -68,6 +68,7 @@ type Options struct {
 func isValueNode(kind string) bool {
 	switch kind {
 	case "identifier", "type_identifier", "decimal_integer_literal",
+		"decimal_floating_point_literal",
 		"string_literal", "escape_sequence", "comment",
 		"true", "false", "void_type", "null",
 		"final_builtin", "const_builtin", "inferred_type", "this",
