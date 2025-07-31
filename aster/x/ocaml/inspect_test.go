3f016e6931fa73f4a03f3f7058fa3220a46b79e6
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-07-31 19:51:06 +0700

    aster/ocaml: extend golden tests to 75

--- aster/x/ocaml/inspect_test.go
+++ aster/x/ocaml/inspect_test.go
@@ -54,8 +54,8 @@ func TestInspect_Golden(t *testing.T) {
 		t.Fatal(err)
 	}
 	sort.Strings(files)
-	if len(files) > 50 {
-		files = files[:50]
+	if len(files) > 75 {
+		files = files[:75]
 	}
 
 	for _, src := range files {
