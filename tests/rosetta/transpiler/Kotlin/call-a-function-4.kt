c53963521eb19fa773d4d43aec2d94d22f670a0d
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-02 13:54:55 +0700

    Fix Kotlin transpiler map assignment

--- tests/rosetta/transpiler/Kotlin/call-a-function-4.kt
+++ tests/rosetta/transpiler/Kotlin/call-a-function-4.kt
@@ -28,8 +28,8 @@ fun gifEncode(out: Any?, img: Any?, opts: MutableMap<String, Int>): Unit {
 }
 
 fun user_main(): Unit {
-    var opts: MutableMap<String, Int> = mutableMapOf<Any?, Any?>() as MutableMap<String, Int>
-    ((opts)["NumColors"] as Int) = 16
+    var opts: MutableMap<String, Int> = mutableMapOf<String, Int>()
+    (opts)["NumColors"] = 16
     gifEncode(null as Any?, null as Any?, opts)
 }
 
