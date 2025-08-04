1a3cbdcdc429aae2dc4a31b4fff11ae3cf866cee
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-04 10:58:04 +0700

    clojure rosetta: add catalan-numbers-2

--- tests/rosetta/transpiler/Clojure/case-sensitivity-of-identifiers.clj
+++ tests/rosetta/transpiler/Clojure/case-sensitivity-of-identifiers.clj
@@ -12,26 +12,25 @@
 
 (declare main)
 
-(declare main_DOG main_Dog main_d main_dog main_pkg_DOG main_pkg_dog)
+(def ^:dynamic main_DOG nil)
+
+(def ^:dynamic main_Dog nil)
+
+(def ^:dynamic main_d nil)
+
+(def ^:dynamic main_dog nil)
+
+(def ^:dynamic main_pkg_DOG nil)
+
+(def ^:dynamic main_pkg_dog nil)
 
 (defn packageSees [packageSees_d1 packageSees_d2 packageSees_d3]
   (try (do (println (str (str (str (str (str "Package sees: " packageSees_d1) " ") packageSees_d2) " ") packageSees_d3)) (throw (ex-info "return" {:v {"pkg_dog" true "Dog" true "pkg_DOG" true}}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
 
 (defn main []
-  (try (do (def main_pkg_dog "Salt") (def main_Dog "Pepper") (def main_pkg_DOG "Mustard") (def main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str "There are " (str (count main_d))) " dogs.\n")) (def main_dog "Benjamin") (def main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str (str (str (str "Main sees:   " main_dog) " ") main_Dog) " ") main_pkg_DOG)) (def main_d (assoc main_d "dog" true)) (def main_d (assoc main_d "Dog" true)) (def main_d (assoc main_d "pkg_DOG" true)) (println (str (str "There are " (str (count main_d))) " dogs.\n")) (def main_Dog "Samba") (def main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str (str (str (str "Main sees:   " main_dog) " ") main_Dog) " ") main_pkg_DOG)) (def main_d (assoc main_d "dog" true)) (def main_d (assoc main_d "Dog" true)) (def main_d (assoc main_d "pkg_DOG" true)) (println (str (str "There are " (str (count main_d))) " dogs.\n")) (def main_DOG "Bernie") (def main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str (str (str (str "Main sees:   " main_dog) " ") main_Dog) " ") main_DOG)) (def main_d (assoc main_d "dog" true)) (def main_d (assoc main_d "Dog" true)) (def main_d (assoc main_d "pkg_DOG" true)) (def main_d (assoc main_d "DOG" true)) (println (str (str "There are " (str (count main_d))) " dogs."))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
+  (binding [main_DOG nil main_Dog nil main_d nil main_dog nil main_pkg_DOG nil main_pkg_dog nil] (try (do (set! main_pkg_dog "Salt") (set! main_Dog "Pepper") (set! main_pkg_DOG "Mustard") (set! main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str "There are " (str (count main_d))) " dogs.\n")) (set! main_dog "Benjamin") (set! main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str (str (str (str "Main sees:   " main_dog) " ") main_Dog) " ") main_pkg_DOG)) (set! main_d (assoc main_d "dog" true)) (set! main_d (assoc main_d "Dog" true)) (set! main_d (assoc main_d "pkg_DOG" true)) (println (str (str "There are " (str (count main_d))) " dogs.\n")) (set! main_Dog "Samba") (set! main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str (str (str (str "Main sees:   " main_dog) " ") main_Dog) " ") main_pkg_DOG)) (set! main_d (assoc main_d "dog" true)) (set! main_d (assoc main_d "Dog" true)) (set! main_d (assoc main_d "pkg_DOG" true)) (println (str (str "There are " (str (count main_d))) " dogs.\n")) (set! main_DOG "Bernie") (set! main_d (packageSees main_pkg_dog main_Dog main_pkg_DOG)) (println (str (str (str (str (str "Main sees:   " main_dog) " ") main_Dog) " ") main_DOG)) (set! main_d (assoc main_d "dog" true)) (set! main_d (assoc main_d "Dog" true)) (set! main_d (assoc main_d "pkg_DOG" true)) (set! main_d (assoc main_d "DOG" true)) (println (str (str "There are " (str (count main_d))) " dogs."))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
 (defn -main []
-  (let [rt (Runtime/getRuntime)
-    start-mem (- (.totalMemory rt) (.freeMemory rt))
-    start (System/nanoTime)]
-      (main)
-      (System/gc)
-      (let [end (System/nanoTime)
-        end-mem (- (.totalMemory rt) (.freeMemory rt))
-        duration-us (quot (- end start) 1000)
-        memory-bytes (Math/abs ^long (- end-mem start-mem))]
-        (println (str "{\n  \"duration_us\": " duration-us ",\n  \"memory_bytes\": " memory-bytes ",\n  \"name\": \"main\"\n}"))
-      )
-    ))
+  (main))
 
 (-main)
--- tests/rosetta/transpiler/Clojure/catalan-numbers-1.clj
+++ tests/rosetta/transpiler/Clojure/catalan-numbers-1.clj
@@ -12,29 +12,22 @@
 
 (declare binom catalan main)
 
-(declare binom_i binom_kk binom_res)
+(def ^:dynamic binom_i nil)
+
+(def ^:dynamic binom_kk nil)
+
+(def ^:dynamic binom_res nil)
 
 (defn binom [binom_n binom_k]
-  (try (do (when (or (< binom_k 0) (> binom_k binom_n)) (throw (ex-info "return" {:v 0}))) (def binom_kk binom_k) (when (> binom_kk (- binom_n binom_kk)) (def binom_kk (- binom_n binom_kk))) (def binom_res 1) (def binom_i 0) (while (< binom_i binom_kk) (do (def binom_res (* binom_res (- binom_n binom_i))) (def binom_i (+ binom_i 1)) (def binom_res (int (/ binom_res binom_i))))) (throw (ex-info "return" {:v binom_res}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
+  (binding [binom_i nil binom_kk nil binom_res nil] (try (do (when (or (< binom_k 0) (> binom_k binom_n)) (throw (ex-info "return" {:v 0}))) (set! binom_kk binom_k) (when (> binom_kk (- binom_n binom_kk)) (set! binom_kk (- binom_n binom_kk))) (set! binom_res 1) (set! binom_i 0) (while (< binom_i binom_kk) (do (set! binom_res (* binom_res (- binom_n binom_i))) (set! binom_i (+ binom_i 1)) (set! binom_res (long (/ binom_res binom_i))))) (throw (ex-info "return" {:v binom_res}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
 (defn catalan [catalan_n]
-  (try (throw (ex-info "return" {:v (int (/ (binom (* 2 catalan_n) catalan_n) (+ catalan_n 1)))})) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
+  (try (throw (ex-info "return" {:v (long (/ (binom (* 2 catalan_n) catalan_n) (+ catalan_n 1)))})) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
 
 (defn main []
   (dotimes [i 15] (println (str (catalan i)))))
 
 (defn -main []
-  (let [rt (Runtime/getRuntime)
-    start-mem (- (.totalMemory rt) (.freeMemory rt))
-    start (System/nanoTime)]
-      (main)
-      (System/gc)
-      (let [end (System/nanoTime)
-        end-mem (- (.totalMemory rt) (.freeMemory rt))
-        duration-us (quot (- end start) 1000)
-        memory-bytes (Math/abs ^long (- end-mem start-mem))]
-        (println (str "{\n  \"duration_us\": " duration-us ",\n  \"memory_bytes\": " memory-bytes ",\n  \"name\": \"main\"\n}"))
-      )
-    ))
+  (main))
 
 (-main)
