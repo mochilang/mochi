a0c69b1ec61143636e2ec0213ad64412054f7a45
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-04 19:18:55 +0700

    feat(clj): transpile more rosetta tasks

--- tests/rosetta/transpiler/Clojure/count-the-coins-2.clj
+++ tests/rosetta/transpiler/Clojure/count-the-coins-2.clj
@@ -5,27 +5,31 @@
 (defn in [x coll]
   (cond (string? coll) (clojure.string/includes? coll x) (map? coll) (contains? coll x) (sequential? coll) (some (fn [e] (= e x)) coll) :else false))
 
+(defn padStart [s w p]
+  (loop [out (str s)] (if (< (count out) w) (recur (str p out)) out)))
+
+(defn indexOf [s sub]
+  (let [idx (clojure.string/index-of s sub)] (if (nil? idx) -1 idx)))
+
+(defn split [s sep]
+  (clojure.string/split s (re-pattern sep)))
+
 (def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
 
 (declare countChange)
 
-(defn countChange [amount]
-  (try (do (def ways []) (def i 0) (while (<= i amount) (do (def ways (conj ways 0)) (def i (+ i 1)))) (def ways (assoc ways 0 1)) (doseq [coin [100 50 25 10 5 1]] (do (def j coin) (while (<= j amount) (do (def ways (assoc ways j (+ (nth ways j) (nth ways (- j coin))))) (def j (+ j 1)))))) (throw (ex-info "return" {:v (nth ways amount)}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
+(def ^:dynamic countChange_i nil)
 
-(def amount 1000)
+(def ^:dynamic countChange_j nil)
+
+(def ^:dynamic countChange_ways nil)
+
+(defn countChange [countChange_amount]
+  (binding [countChange_i nil countChange_j nil countChange_ways nil] (try (do (set! countChange_ways []) (set! countChange_i 0) (while (<= countChange_i countChange_amount) (do (set! countChange_ways (conj countChange_ways 0)) (set! countChange_i (+ countChange_i 1)))) (set! countChange_ways (assoc countChange_ways 0 1)) (doseq [coin [100 50 25 10 5 1]] (do (set! countChange_j coin) (while (<= countChange_j countChange_amount) (do (set! countChange_ways (assoc countChange_ways countChange_j (+ (nth countChange_ways countChange_j) (nth countChange_ways (- countChange_j coin))))) (set! countChange_j (+ countChange_j 1)))))) (throw (ex-info "return" {:v (nth countChange_ways countChange_amount)}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
+
+(def ^:dynamic main_amount 1000)
 
 (defn -main []
-  (let [rt (Runtime/getRuntime)
-    start-mem (- (.totalMemory rt) (.freeMemory rt))
-    start (System/nanoTime)]
-      (println (str (str (str "amount, ways to make change: " (str amount)) " ") (str (countChange amount))))
-      (System/gc)
-      (let [end (System/nanoTime)
-        end-mem (- (.totalMemory rt) (.freeMemory rt))
-        duration-us (quot (- end start) 1000)
-        memory-bytes (Math/abs ^long (- end-mem start-mem))]
-        (println (str "{\n  \"duration_us\": " duration-us ",\n  \"memory_bytes\": " memory-bytes ",\n  \"name\": \"main\"\n}"))
-      )
-    ))
+  (println (str (str (str "amount, ways to make change: " (str main_amount)) " ") (str (countChange main_amount)))))
 
 (-main)
