f4845964bc3119c070426f768aa3f23e1001ff6f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-06 18:00:12 +0700

    cs transpiler: add list concat

--- transpiler/x/cs/algorithms_test.go
+++ transpiler/x/cs/algorithms_test.go
@@ -101,7 +101,7 @@ func TestCSTranspiler_Algorithms_Golden(t *testing.T) {
 			if data, err := os.ReadFile(strings.TrimSuffix(src, ".mochi") + ".in"); err == nil {
 				cmd.Stdin = bytes.NewReader(data)
 			}
-			want, _ := os.ReadFile(strings.TrimSuffix(src, ".mochi") + ".out")
+			want, _ := os.ReadFile(outPath)
 			want = bytes.TrimSpace(want)
 			out, err := cmd.CombinedOutput()
 			got := bytes.TrimSpace(out)
