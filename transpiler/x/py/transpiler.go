41c10589b2f536eb5d79235ae6258f9df7f0d0df
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-08 09:00:44 +0700

    py: respect user-defined contains and update algorithms

--- transpiler/x/py/transpiler.go
+++ transpiler/x/py/transpiler.go
@@ -4997,10 +4997,10 @@ func convertPrimary(p *parser.Primary) (Expr, error) {
 			if len(args) == 2 {
 				return &BinaryExpr{Left: args[0], Op: "*", Right: args[1]}, nil
 			}
-		case "contains":
-			if len(args) == 2 {
-				return &BinaryExpr{Left: args[1], Op: "in", Right: args[0]}, nil
-			}
+               case "contains":
+                       if !hasFunc && len(args) == 2 {
+                               return &BinaryExpr{Left: args[1], Op: "in", Right: args[0]}, nil
+                       }
 		case "parseIntStr":
 			if len(args) == 2 {
 				return &CallExpr{Func: &Name{Name: "int"}, Args: []Expr{args[0], args[1]}}, nil
