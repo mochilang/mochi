2625f1203a2e52d4091c71dffce3800cdd55245a
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-09 18:17:46 +0700

    clj: handle float division in algorithms

---
+++ tests/algorithms/x/Clojure/maths/special_numbers/hexagonal_number.clj
@@ -0,0 +1,43 @@
+(ns main (:refer-clojure :exclude [hexagonal]))
+
+(require 'clojure.set)
+
+(defn in [x coll]
+  (cond (string? coll) (clojure.string/includes? coll x) (map? coll) (contains? coll x) (sequential? coll) (some (fn [e] (= e x)) coll) :else false))
+
+(defn padStart [s w p]
+  (loop [out (str s)] (if (< (count out) w) (recur (str p out)) out)))
+
+(defn indexOf [s sub]
+  (let [idx (clojure.string/index-of s sub)] (if (nil? idx) -1 idx)))
+
+(defn split [s sep]
+  (clojure.string/split s (re-pattern sep)))
+
+(defn toi [s]
+  (Integer/parseInt (str s)))
+
+(def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
+
+(declare hexagonal)
+
+(defn hexagonal [hexagonal_n]
+  (try (do (when (< hexagonal_n 1) (throw (Exception. "Input must be a positive integer"))) (throw (ex-info "return" {:v (* hexagonal_n (- (* 2 hexagonal_n) 1))}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
+
+(def ^:dynamic main_samples [4 11 22])
+
+(defn -main []
+  (let [rt (Runtime/getRuntime)
+    start-mem (- (.totalMemory rt) (.freeMemory rt))
+    start (System/nanoTime)]
+      (doseq [s main_samples] (println (hexagonal s)))
+      (System/gc)
+      (let [end (System/nanoTime)
+        end-mem (- (.totalMemory rt) (.freeMemory rt))
+        duration-us (quot (- end start) 1000)
+        memory-bytes (Math/abs ^long (- end-mem start-mem))]
+        (println (str "{\n  \"duration_us\": " duration-us ",\n  \"memory_bytes\": " memory-bytes ",\n  \"name\": \"main\"\n}"))
+      )
+    ))
+
+(-main)
