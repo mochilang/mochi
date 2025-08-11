548b01b740c986c992d2672dc089e8b315f08955
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-11 17:57:03 +0700

    Revert "support read_file in C# transpiler"
    This reverts commit 2f8e5e725e2cd94cb9e3c28c2035276902a69137.

--- transpiler/x/cs/algorithms_test.go
+++ transpiler/x/cs/algorithms_test.go
@@ -97,8 +97,7 @@ func TestCSTranspiler_Algorithms_Golden(t *testing.T) {
 				t.Fatalf("write temp code: %v", err)
 			}
 			cmd := exec.Command("dotnet", "run", "--project", proj)
-			rootEnv := "MOCHI_ROOT=" + filepath.Dir(src)
-			cmd.Env = append(os.Environ(), "DOTNET_NOLOGO=1", "DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1", "MOCHI_BENCHMARK=1", rootEnv)
+			cmd.Env = append(os.Environ(), "DOTNET_NOLOGO=1", "DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1", "MOCHI_BENCHMARK=1")
 			if data, err := os.ReadFile(strings.TrimSuffix(src, ".mochi") + ".in"); err == nil {
 				cmd.Stdin = bytes.NewReader(data)
 			}
