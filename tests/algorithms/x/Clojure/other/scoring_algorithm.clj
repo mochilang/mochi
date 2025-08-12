923db644cb9058fce70bf543db3442329a28d987
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-12 16:22:21 +0700

    clj: support to_float and safe top-level lets

--- tests/algorithms/x/Clojure/other/scoring_algorithm.clj
+++ tests/algorithms/x/Clojure/other/scoring_algorithm.clj
@@ -86,21 +86,22 @@
   (binding [count_v nil generate_final_scores_final_scores nil generate_final_scores_i nil generate_final_scores_j nil generate_final_scores_slist nil] (try (do (set! count_v (count (nth generate_final_scores_score_lists 0))) (set! generate_final_scores_final_scores []) (set! generate_final_scores_i 0) (while (< generate_final_scores_i count_v) (do (set! generate_final_scores_final_scores (conj generate_final_scores_final_scores 0.0)) (set! generate_final_scores_i (+ generate_final_scores_i 1)))) (set! generate_final_scores_i 0) (while (< generate_final_scores_i (count generate_final_scores_score_lists)) (do (set! generate_final_scores_slist (nth generate_final_scores_score_lists generate_final_scores_i)) (set! generate_final_scores_j 0) (while (< generate_final_scores_j (count generate_final_scores_slist)) (do (set! generate_final_scores_final_scores (assoc generate_final_scores_final_scores generate_final_scores_j (+ (nth generate_final_scores_final_scores generate_final_scores_j) (nth generate_final_scores_slist generate_final_scores_j)))) (set! generate_final_scores_j (+ generate_final_scores_j 1)))) (set! generate_final_scores_i (+ generate_final_scores_i 1)))) (throw (ex-info "return" {:v generate_final_scores_final_scores}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
 (defn procentual_proximity [procentual_proximity_source_data_p procentual_proximity_weights]
-  (binding [procentual_proximity_data_lists nil procentual_proximity_final_scores nil procentual_proximity_i nil procentual_proximity_score_lists nil procentual_proximity_source_data nil] (try (do (set! procentual_proximity_source_data procentual_proximity_source_data_p) (set! procentual_proximity_data_lists (get_data procentual_proximity_source_data)) (set! procentual_proximity_score_lists (calculate_each_score procentual_proximity_data_lists procentual_proximity_weights)) (set! procentual_proximity_final_scores (generate_final_scores procentual_proximity_score_lists)) (set! procentual_proximity_i 0) (while (< procentual_proximity_i (count procentual_proximity_final_scores)) (do (set! procentual_proximity_source_data (assoc procentual_proximity_source_data procentual_proximity_i (conj (nth procentual_proximity_source_data procentual_proximity_i) (nth procentual_proximity_final_scores procentual_proximity_i)))) (set! procentual_proximity_i (+ procentual_proximity_i 1)))) (throw (ex-info "return" {:v procentual_proximity_source_data}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
+  (binding [procentual_proximity_source_data procentual_proximity_source_data_p procentual_proximity_data_lists nil procentual_proximity_final_scores nil procentual_proximity_i nil procentual_proximity_score_lists nil] (try (do (set! procentual_proximity_data_lists (get_data procentual_proximity_source_data)) (set! procentual_proximity_score_lists (calculate_each_score procentual_proximity_data_lists procentual_proximity_weights)) (set! procentual_proximity_final_scores (generate_final_scores procentual_proximity_score_lists)) (set! procentual_proximity_i 0) (while (< procentual_proximity_i (count procentual_proximity_final_scores)) (do (set! procentual_proximity_source_data (assoc procentual_proximity_source_data procentual_proximity_i (conj (nth procentual_proximity_source_data procentual_proximity_i) (nth procentual_proximity_final_scores procentual_proximity_i)))) (set! procentual_proximity_i (+ procentual_proximity_i 1)))) (throw (ex-info "return" {:v procentual_proximity_source_data}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))) (finally (alter-var-root (var procentual_proximity_source_data) (constantly procentual_proximity_source_data))))))
 
 (def ^:dynamic main_vehicles [])
 
 (def ^:dynamic main_weights [0 0 1])
 
-(def ^:dynamic main_result (procentual_proximity main_vehicles main_weights))
+(def ^:dynamic main_result nil)
 
 (defn -main []
   (let [rt (Runtime/getRuntime)
     start-mem (- (.totalMemory rt) (.freeMemory rt))
     start (System/nanoTime)]
-      (def main_vehicles (conj main_vehicles [20.0 60.0 2012.0]))
-      (def main_vehicles (conj main_vehicles [23.0 90.0 2015.0]))
-      (def main_vehicles (conj main_vehicles [22.0 50.0 2011.0]))
+      (alter-var-root (var main_vehicles) (constantly (conj main_vehicles [20.0 60.0 2012.0])))
+      (alter-var-root (var main_vehicles) (constantly (conj main_vehicles [23.0 90.0 2015.0])))
+      (alter-var-root (var main_vehicles) (constantly (conj main_vehicles [22.0 50.0 2011.0])))
+      (alter-var-root (var main_result) (constantly (let [__res (procentual_proximity main_vehicles main_weights)] (do (alter-var-root (var main_vehicles) (constantly procentual_proximity_source_data)) __res))))
       (println (str main_result))
       (System/gc)
       (let [end (System/nanoTime)
