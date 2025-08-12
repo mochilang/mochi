888dd75309dc3bd3cf06f4a467cc81d733d609e5
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-12 11:02:25 +0700

    chore(clj): add algorithm outputs for indices 750-800

---
+++ tests/algorithms/x/Clojure/physics/newtons_second_law_of_motion.clj
@@ -0,0 +1,50 @@
+(ns main (:refer-clojure :exclude [newtons_second_law_of_motion]))
+
+(require 'clojure.set)
+
+(defn in [x coll]
+  (cond (string? coll) (clojure.string/includes? coll x) (map? coll) (contains? coll x) (sequential? coll) (some (fn [e] (= e x)) coll) :else false))
+
+(defn padStart [s w p]
+  (loop [out (str s)] (if (< (count out) w) (recur (str p out)) out)))
+
+(defn indexOf [s sub]
+  (let [idx (clojure.string/index-of s sub)] (if (nil? idx) -1 idx)))
+
+(defn split [s sep]
+  (clojure.string/split s (re-pattern sep)))
+
+(defn toi [s]
+  (Integer/parseInt (str s)))
+
+(defn _fetch [url]
+  {:data [{:from "" :intensity {:actual 0 :forecast 0 :index ""} :to ""}]})
+
+(def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
+
+(declare newtons_second_law_of_motion)
+
+(defn newtons_second_law_of_motion [newtons_second_law_of_motion_mass newtons_second_law_of_motion_acceleration]
+  (try (if (or (< newtons_second_law_of_motion_mass 0.0) (< newtons_second_law_of_motion_acceleration 0.0)) 0.0 (* newtons_second_law_of_motion_mass newtons_second_law_of_motion_acceleration)) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e)))))
+
+(def ^:dynamic main_mass 12.5)
+
+(def ^:dynamic main_acceleration 10.0)
+
+(def ^:dynamic main_force (newtons_second_law_of_motion main_mass main_acceleration))
+
+(defn -main []
+  (let [rt (Runtime/getRuntime)
+    start-mem (- (.totalMemory rt) (.freeMemory rt))
+    start (System/nanoTime)]
+      (println (str (str "The force is " (str main_force)) " N"))
+      (System/gc)
+      (let [end (System/nanoTime)
+        end-mem (- (.totalMemory rt) (.freeMemory rt))
+        duration-us (quot (- end start) 1000)
+        memory-bytes (Math/abs ^long (- end-mem start-mem))]
+        (println (str "{\n  \"duration_us\": " duration-us ",\n  \"memory_bytes\": " memory-bytes ",\n  \"name\": \"main\"\n}"))
+      )
+    ))
+
+(-main)
