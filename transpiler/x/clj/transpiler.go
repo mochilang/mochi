cefcee4fe09eea8950f31db4f119dbae9725f95a
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-14 15:58:29 +0700

    clj: fix for-loop collection handling

--- transpiler/x/clj/transpiler.go
+++ transpiler/x/clj/transpiler.go
@@ -1733,17 +1733,17 @@ func isStringNode(n Node) bool {
 				}
 			}
 		}
-        case Symbol:
-                if stringListVars != nil {
-                        if stringListVars[string(t)] {
-                                return false
-                        }
-                }
-                if stringVars != nil {
-                        if stringVars[string(t)] {
-                                return true
-                        }
-                }
+	case Symbol:
+		if stringListVars != nil {
+			if stringListVars[string(t)] {
+				return false
+			}
+		}
+		if stringVars != nil {
+			if stringVars[string(t)] {
+				return true
+			}
+		}
 		if transpileEnv != nil {
 			name := string(t)
 			if typ, err := transpileEnv.GetVar(name); err == nil {
@@ -1848,22 +1848,22 @@ func isMapNode(n Node) bool {
 	switch t := n.(type) {
 	case *Map:
 		return true
-        case *List:
-                if len(t.Elems) > 0 {
-                        if sym, ok := t.Elems[0].(Symbol); ok {
-                                if sym == "hash-map" {
-                                        return true
-                                }
-                                if sym == "get" && len(t.Elems) >= 2 {
-                                        if isMapNode(t.Elems[1]) {
-                                                return true
-                                        }
-                                }
-                        }
-                        if _, ok := t.Elems[0].(Keyword); ok {
-                                return true
-                        }
-                }
+	case *List:
+		if len(t.Elems) > 0 {
+			if sym, ok := t.Elems[0].(Symbol); ok {
+				if sym == "hash-map" {
+					return true
+				}
+				if sym == "get" && len(t.Elems) >= 2 {
+					if isMapNode(t.Elems[1]) {
+						return true
+					}
+				}
+			}
+			if _, ok := t.Elems[0].(Keyword); ok {
+				return true
+			}
+		}
 	case Symbol:
 		if mapVars != nil {
 			if mapVars[string(t)] {
@@ -2756,24 +2756,24 @@ func transpileCall(c *parser.CallExpr) (Node, error) {
 				return nil, fmt.Errorf("input expects no args")
 			}
 			return &List{Elems: []Node{Symbol("read-line")}}, nil
-                case "abs":
-                        if len(c.Args) != 1 {
-                                return nil, fmt.Errorf("abs expects 1 arg")
-                        }
-                        arg, err := transpileExpr(c.Args[0])
-                        if err != nil {
-                                return nil, err
-                        }
-                        return &List{Elems: []Node{Symbol("Math/abs"), arg}}, nil
-                case "floor":
-                        if len(c.Args) != 1 {
-                                return nil, fmt.Errorf("floor expects 1 arg")
-                        }
-                        arg, err := transpileExpr(c.Args[0])
-                        if err != nil {
-                                return nil, err
-                        }
-                        return &List{Elems: []Node{Symbol("Math/floor"), arg}}, nil
+		case "abs":
+			if len(c.Args) != 1 {
+				return nil, fmt.Errorf("abs expects 1 arg")
+			}
+			arg, err := transpileExpr(c.Args[0])
+			if err != nil {
+				return nil, err
+			}
+			return &List{Elems: []Node{Symbol("Math/abs"), arg}}, nil
+		case "floor":
+			if len(c.Args) != 1 {
+				return nil, fmt.Errorf("floor expects 1 arg")
+			}
+			arg, err := transpileExpr(c.Args[0])
+			if err != nil {
+				return nil, err
+			}
+			return &List{Elems: []Node{Symbol("Math/floor"), arg}}, nil
 		case "append":
 			elems = append(elems, Symbol("conj"))
 		case "slice":
@@ -3715,7 +3715,8 @@ func transpileForStmt(f *parser.ForStmt) (Node, error) {
 		if err != nil {
 			return nil, err
 		}
-		if isMapNode(iter) {
+		typ := types.TypeOfExprBasic(f.Source, transpileEnv)
+		if _, ok := typ.(types.MapType); ok {
 			seq = &List{Elems: []Node{Symbol("keys"), iter}}
 		} else {
 			seq = iter
