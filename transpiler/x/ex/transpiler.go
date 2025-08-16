458786f4672b8163cef178d5fd10caa7ef50018f
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-16 12:15:26 +0700

    ex transpiler: suppress idx warnings

--- transpiler/x/ex/transpiler.go
+++ transpiler/x/ex/transpiler.go
@@ -144,7 +144,7 @@ func uniqueWhileName() string {
 // counter that is typically not used after the loop ends.
 func isLoopCounter(name string) bool {
 	switch name {
-	case "i", "j", "k", "l":
+	case "i", "j", "k", "l", "idx":
 		return true
 	default:
 		return false
