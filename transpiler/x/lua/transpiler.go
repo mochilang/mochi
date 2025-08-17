0508ec5817516f6d7abf31631091719408437eed
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-17 12:30:01 +0700

    lua: fix int casting and update collinear benchmark

--- transpiler/x/lua/transpiler.go
+++ transpiler/x/lua/transpiler.go
@@ -706,18 +706,18 @@ func (c *CallExpr) emit(w io.Writer) {
 			io.WriteString(w, "nil")
 		}
 		io.WriteString(w, "))")
-	case "int":
-		io.WriteString(w, "(math.modf(tonumber(")
-		if len(c.Args) > 0 {
-			c.Args[0].emit(w)
-		}
-		io.WriteString(w, ") or 0))")
-	case "toi":
-		io.WriteString(w, "(math.modf(tonumber(")
-		if len(c.Args) > 0 {
-			c.Args[0].emit(w)
-		}
-		io.WriteString(w, ") or 0))")
+       case "int":
+               io.WriteString(w, "(function(v) if v >= 0 then return math.floor(v) else return math.ceil(v) end end)((tonumber(")
+               if len(c.Args) > 0 {
+                       c.Args[0].emit(w)
+               }
+               io.WriteString(w, ") or 0))")
+       case "toi":
+               io.WriteString(w, "(function(v) if v >= 0 then return math.floor(v) else return math.ceil(v) end end)((tonumber(")
+               if len(c.Args) > 0 {
+                       c.Args[0].emit(w)
+               }
+               io.WriteString(w, ") or 0))")
 	case "float":
 		io.WriteString(w, "tonumber(")
 		if len(c.Args) > 0 {
