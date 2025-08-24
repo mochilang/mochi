f2c8fd0866ae5b3d86303516fa3333841bd8b44c
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-24 16:57:55 +0700

    clj: handle ranged loops with control

--- tests/algorithms/x/Clojure/conversions/weight_conversion.clj
+++ tests/algorithms/x/Clojure/conversions/weight_conversion.clj
@@ -18,6 +18,20 @@
 (defn split [s sep]
   (clojure.string/split s (re-pattern sep)))
 
+(defn toi [s]
+  (int (Double/valueOf (str s))))
+
+(defn _ord [s]
+  (int (first s)))
+
+(defn mochi_str [v]
+  (cond (float? v) (let [s (str v)] (if (clojure.string/ends-with? s ".0") (subs s 0 (- (count s) 2)) s)) :else (str v)))
+
+(defn _fetch [url]
+  {:data [{:from "" :intensity {:actual 0 :forecast 0 :index ""} :to ""}]})
+
+(def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
+
 (def ^:dynamic pow10_i nil)
 
 (def ^:dynamic pow10_result nil)
@@ -26,16 +40,16 @@
 
 (def ^:dynamic weight_conversion_has_to nil)
 
-(def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
-
 (declare pow10 weight_conversion)
 
+(declare _read_file)
+
 (defn pow10 [pow10_exp]
-  (binding [pow10_i nil pow10_result nil] (try (do (set! pow10_result 1.0) (if (>= pow10_exp 0) (do (set! pow10_i 0) (while (< pow10_i pow10_exp) (do (set! pow10_result (* pow10_result 10.0)) (set! pow10_i (+ pow10_i 1))))) (do (set! pow10_i 0) (while (< pow10_i (- 0 pow10_exp)) (do (set! pow10_result (/ pow10_result 10.0)) (set! pow10_i (+ pow10_i 1)))))) (throw (ex-info "return" {:v pow10_result}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
+  (binding [pow10_i nil pow10_result nil] (try (do (set! pow10_result 1.0) (if (>= pow10_exp 0) (do (set! pow10_i 0) (while (< pow10_i pow10_exp) (do (set! pow10_result (* pow10_result 10.0)) (set! pow10_i (+ pow10_i 1))))) (do (set! pow10_i 0) (while (< pow10_i (- pow10_exp)) (do (set! pow10_result (/ pow10_result 10.0)) (set! pow10_i (+ pow10_i 1)))))) (throw (ex-info "return" {:v pow10_result}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
-(def ^:dynamic main_KILOGRAM_CHART {"kilogram" 1.0 "gram" 1000.0 "milligram" 1000000.0 "metric-ton" 0.001 "long-ton" 0.0009842073 "short-ton" 0.0011023122 "pound" 2.2046244202 "stone" 0.1574731728 "ounce" 35.273990723 "carrat" 5000.0 "atomic-mass-unit" (* 6.022136652 (pow10 26))})
+(def ^:dynamic main_KILOGRAM_CHART nil)
 
-(def ^:dynamic main_WEIGHT_TYPE_CHART {"kilogram" 1.0 "gram" 0.001 "milligram" 0.000001 "metric-ton" 1000.0 "long-ton" 1016.04608 "short-ton" 907.184 "pound" 0.453592 "stone" 6.35029 "ounce" 0.0283495 "carrat" 0.0002 "atomic-mass-unit" (* 1.660540199 (pow10 (- 27)))})
+(def ^:dynamic main_WEIGHT_TYPE_CHART nil)
 
 (defn weight_conversion [weight_conversion_from_type weight_conversion_to_type weight_conversion_value]
   (binding [weight_conversion_has_from nil weight_conversion_has_to nil] (try (do (set! weight_conversion_has_to (in weight_conversion_to_type main_KILOGRAM_CHART)) (set! weight_conversion_has_from (in weight_conversion_from_type main_WEIGHT_TYPE_CHART)) (when (and weight_conversion_has_to weight_conversion_has_from) (throw (ex-info "return" {:v (* (* weight_conversion_value (get main_KILOGRAM_CHART weight_conversion_to_type)) (get main_WEIGHT_TYPE_CHART weight_conversion_from_type))}))) (println "Invalid 'from_type' or 'to_type'") (throw (ex-info "return" {:v 0.0}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
@@ -44,6 +58,8 @@
   (let [rt (Runtime/getRuntime)
     start-mem (- (.totalMemory rt) (.freeMemory rt))
     start (System/nanoTime)]
+      (alter-var-root (var main_KILOGRAM_CHART) (constantly {"atomic-mass-unit" (* 6.022136652 (pow10 26)) "carrat" 5000.0 "gram" 1000.0 "kilogram" 1.0 "long-ton" 0.0009842073 "metric-ton" 0.001 "milligram" 1000000.0 "ounce" 35.273990723 "pound" 2.2046244202 "short-ton" 0.0011023122 "stone" 0.1574731728}))
+      (alter-var-root (var main_WEIGHT_TYPE_CHART) (constantly {"atomic-mass-unit" (* 1.660540199 (pow10 (- 27))) "carrat" 0.0002 "gram" 0.001 "kilogram" 1.0 "long-ton" 1016.04608 "metric-ton" 1000.0 "milligram" 0.000001 "ounce" 0.0283495 "pound" 0.453592 "short-ton" 907.184 "stone" 6.35029}))
       (println (weight_conversion "kilogram" "gram" 1.0))
       (println (weight_conversion "gram" "pound" 3.0))
       (println (weight_conversion "ounce" "kilogram" 3.0))
--- tests/algorithms/x/Clojure/data_compression/run_length_encoding.clj
+++ tests/algorithms/x/Clojure/data_compression/run_length_encoding.clj
@@ -14,9 +14,23 @@
 (defn split [s sep]
   (clojure.string/split s (re-pattern sep)))
 
+(defn toi [s]
+  (int (Double/valueOf (str s))))
+
+(defn _ord [s]
+  (int (first s)))
+
+(defn mochi_str [v]
+  (cond (float? v) (let [s (str v)] (if (clojure.string/ends-with? s ".0") (subs s 0 (- (count s) 2)) s)) :else (str v)))
+
+(defn _fetch [url]
+  {:data [{:from "" :intensity {:actual 0 :forecast 0 :index ""} :to ""}]})
+
 (def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
 
 (declare run_length_encode run_length_decode)
+
+(declare _read_file)
 
 (def ^:dynamic count_v nil)
 
@@ -35,31 +49,37 @@
 (def ^:dynamic run_length_encode_i nil)
 
 (defn run_length_encode [run_length_encode_text]
-  (binding [count_v nil run_length_encode_encoded nil run_length_encode_i nil] (try (do (when (= (count run_length_encode_text) 0) (throw (ex-info "return" {:v ""}))) (set! run_length_encode_encoded "") (set! count_v 1) (set! run_length_encode_i 0) (while (< run_length_encode_i (count run_length_encode_text)) (do (if (and (< (+ run_length_encode_i 1) (count run_length_encode_text)) (= (nth run_length_encode_text run_length_encode_i) (nth run_length_encode_text (+ run_length_encode_i 1)))) (set! count_v (+ count_v 1)) (do (set! run_length_encode_encoded (str (str run_length_encode_encoded (nth run_length_encode_text run_length_encode_i)) (str count_v))) (set! count_v 1))) (set! run_length_encode_i (+ run_length_encode_i 1)))) (throw (ex-info "return" {:v run_length_encode_encoded}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
+  (binding [count_v nil run_length_encode_encoded nil run_length_encode_i nil] (try (do (when (= (count run_length_encode_text) 0) (throw (ex-info "return" {:v ""}))) (set! run_length_encode_encoded "") (set! count_v 1) (set! run_length_encode_i 0) (while (< run_length_encode_i (count run_length_encode_text)) (do (if (and (< (+ run_length_encode_i 1) (count run_length_encode_text)) (= (subs run_length_encode_text run_length_encode_i (+ run_length_encode_i 1)) (subs run_length_encode_text (+ run_length_encode_i 1) (+ (+ run_length_encode_i 1) 1)))) (set! count_v (+ count_v 1)) (do (set! run_length_encode_encoded (str (str run_length_encode_encoded (subs run_length_encode_text run_length_encode_i (+ run_length_encode_i 1))) (mochi_str count_v))) (set! count_v 1))) (set! run_length_encode_i (+ run_length_encode_i 1)))) (throw (ex-info "return" {:v run_length_encode_encoded}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
 (defn run_length_decode [run_length_decode_encoded]
-  (binding [count_v nil run_length_decode_ch nil run_length_decode_i nil run_length_decode_j nil run_length_decode_num_str nil run_length_decode_res nil] (try (do (set! run_length_decode_res "") (set! run_length_decode_i 0) (while (< run_length_decode_i (count run_length_decode_encoded)) (do (set! run_length_decode_ch (nth run_length_decode_encoded run_length_decode_i)) (set! run_length_decode_i (+ run_length_decode_i 1)) (set! run_length_decode_num_str "") (while (and (and (< run_length_decode_i (count run_length_decode_encoded)) (>= (compare (nth run_length_decode_encoded run_length_decode_i) "0") 0)) (<= (compare (nth run_length_decode_encoded run_length_decode_i) "9") 0)) (do (set! run_length_decode_num_str (str run_length_decode_num_str (nth run_length_decode_encoded run_length_decode_i))) (set! run_length_decode_i (+ run_length_decode_i 1)))) (set! count_v (Integer/parseInt run_length_decode_num_str)) (set! run_length_decode_j 0) (while (< run_length_decode_j count_v) (do (set! run_length_decode_res (str run_length_decode_res run_length_decode_ch)) (set! run_length_decode_j (+ run_length_decode_j 1)))))) (throw (ex-info "return" {:v run_length_decode_res}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
+  (binding [count_v nil run_length_decode_ch nil run_length_decode_i nil run_length_decode_j nil run_length_decode_num_str nil run_length_decode_res nil] (try (do (set! run_length_decode_res "") (set! run_length_decode_i 0) (while (< run_length_decode_i (count run_length_decode_encoded)) (do (set! run_length_decode_ch (subs run_length_decode_encoded run_length_decode_i (+ run_length_decode_i 1))) (set! run_length_decode_i (+ run_length_decode_i 1)) (set! run_length_decode_num_str "") (while (and (and (< run_length_decode_i (count run_length_decode_encoded)) (>= (compare (subs run_length_decode_encoded run_length_decode_i (+ run_length_decode_i 1)) "0") 0)) (<= (compare (subs run_length_decode_encoded run_length_decode_i (+ run_length_decode_i 1)) "9") 0)) (do (set! run_length_decode_num_str (str run_length_decode_num_str (subs run_length_decode_encoded run_length_decode_i (+ run_length_decode_i 1)))) (set! run_length_decode_i (+ run_length_decode_i 1)))) (set! count_v (toi run_length_decode_num_str)) (set! run_length_decode_j 0) (while (< run_length_decode_j count_v) (do (set! run_length_decode_res (str run_length_decode_res run_length_decode_ch)) (set! run_length_decode_j (+ run_length_decode_j 1)))))) (throw (ex-info "return" {:v run_length_decode_res}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
-(def ^:dynamic main_example1 "AAAABBBCCDAA")
+(def ^:dynamic main_example1 nil)
 
-(def ^:dynamic main_encoded1 (run_length_encode main_example1))
+(def ^:dynamic main_encoded1 nil)
 
-(def ^:dynamic main_example2 "A")
+(def ^:dynamic main_example2 nil)
 
-(def ^:dynamic main_encoded2 (run_length_encode main_example2))
+(def ^:dynamic main_encoded2 nil)
 
-(def ^:dynamic main_example3 "AAADDDDDDFFFCCCAAVVVV")
+(def ^:dynamic main_example3 nil)
 
-(def ^:dynamic main_encoded3 (run_length_encode main_example3))
+(def ^:dynamic main_encoded3 nil)
 
 (defn -main []
   (let [rt (Runtime/getRuntime)
     start-mem (- (.totalMemory rt) (.freeMemory rt))
     start (System/nanoTime)]
+      (alter-var-root (var main_example1) (constantly "AAAABBBCCDAA"))
+      (alter-var-root (var main_encoded1) (constantly (run_length_encode main_example1)))
       (println main_encoded1)
       (println (run_length_decode main_encoded1))
+      (alter-var-root (var main_example2) (constantly "A"))
+      (alter-var-root (var main_encoded2) (constantly (run_length_encode main_example2)))
       (println main_encoded2)
       (println (run_length_decode main_encoded2))
+      (alter-var-root (var main_example3) (constantly "AAADDDDDDFFFCCCAAVVVV"))
+      (alter-var-root (var main_encoded3) (constantly (run_length_encode main_example3)))
       (println main_encoded3)
       (println (run_length_decode main_encoded3))
       (System/gc)
