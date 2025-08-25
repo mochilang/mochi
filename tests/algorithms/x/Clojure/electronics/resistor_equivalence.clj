ffba74ec4940a3acea7249a61c2f13cb844b183c
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-25 19:03:57 +0700

    feat(clj): improve char literal handling

--- tests/algorithms/x/Clojure/electronics/resistor_equivalence.clj
+++ tests/algorithms/x/Clojure/electronics/resistor_equivalence.clj
@@ -15,7 +15,13 @@
   (clojure.string/split s (re-pattern sep)))
 
 (defn toi [s]
-  (Integer/parseInt (str s)))
+  (int (Double/valueOf (str s))))
+
+(defn _ord [s]
+  (int (first s)))
+
+(defn mochi_str [v]
+  (cond (float? v) (let [s (str v)] (if (clojure.string/ends-with? s ".0") (subs s 0 (- (count s) 2)) s)) :else (str v)))
 
 (defn _fetch [url]
   {:data [{:from "" :intensity {:actual 0 :forecast 0 :index ""} :to ""}]})
@@ -23,6 +29,8 @@
 (def nowSeed (atom (let [s (System/getenv "MOCHI_NOW_SEED")] (if (and s (not (= s ""))) (Integer/parseInt s) 0))))
 
 (declare resistor_parallel resistor_series main)
+
+(declare _read_file)
 
 (def ^:dynamic main_resistors nil)
 
@@ -39,13 +47,13 @@
 (def ^:dynamic resistor_series_sum nil)
 
 (defn resistor_parallel [resistor_parallel_resistors]
-  (binding [resistor_parallel_i nil resistor_parallel_r nil resistor_parallel_sum nil] (try (do (set! resistor_parallel_sum 0.0) (set! resistor_parallel_i 0) (while (< resistor_parallel_i (count resistor_parallel_resistors)) (do (set! resistor_parallel_r (nth resistor_parallel_resistors resistor_parallel_i)) (when (<= resistor_parallel_r 0.0) (throw (Exception. (str (str "Resistor at index " (str resistor_parallel_i)) " has a negative or zero value!")))) (set! resistor_parallel_sum (+ resistor_parallel_sum (/ 1.0 resistor_parallel_r))) (set! resistor_parallel_i (+ resistor_parallel_i 1)))) (throw (ex-info "return" {:v (/ 1.0 resistor_parallel_sum)}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
+  (binding [resistor_parallel_i nil resistor_parallel_r nil resistor_parallel_sum nil] (try (do (set! resistor_parallel_sum 0.0) (set! resistor_parallel_i 0) (while (< resistor_parallel_i (count resistor_parallel_resistors)) (do (set! resistor_parallel_r (nth resistor_parallel_resistors resistor_parallel_i)) (when (<= resistor_parallel_r 0.0) (throw (Exception. (str (str "Resistor at index " (mochi_str resistor_parallel_i)) " has a negative or zero value!")))) (set! resistor_parallel_sum (+ resistor_parallel_sum (/ 1.0 resistor_parallel_r))) (set! resistor_parallel_i (+ resistor_parallel_i 1)))) (throw (ex-info "return" {:v (/ 1.0 resistor_parallel_sum)}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
 (defn resistor_series [resistor_series_resistors]
-  (binding [resistor_series_i nil resistor_series_r nil resistor_series_sum nil] (try (do (set! resistor_series_sum 0.0) (set! resistor_series_i 0) (while (< resistor_series_i (count resistor_series_resistors)) (do (set! resistor_series_r (nth resistor_series_resistors resistor_series_i)) (when (< resistor_series_r 0.0) (throw (Exception. (str (str "Resistor at index " (str resistor_series_i)) " has a negative value!")))) (set! resistor_series_sum (+ resistor_series_sum resistor_series_r)) (set! resistor_series_i (+ resistor_series_i 1)))) (throw (ex-info "return" {:v resistor_series_sum}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
+  (binding [resistor_series_i nil resistor_series_r nil resistor_series_sum nil] (try (do (set! resistor_series_sum 0.0) (set! resistor_series_i 0) (while (< resistor_series_i (count resistor_series_resistors)) (do (set! resistor_series_r (nth resistor_series_resistors resistor_series_i)) (when (< resistor_series_r 0.0) (throw (Exception. (str (str "Resistor at index " (mochi_str resistor_series_i)) " has a negative value!")))) (set! resistor_series_sum (+ resistor_series_sum resistor_series_r)) (set! resistor_series_i (+ resistor_series_i 1)))) (throw (ex-info "return" {:v resistor_series_sum}))) (catch clojure.lang.ExceptionInfo e (if (= (ex-message e) "return") (get (ex-data e) :v) (throw e))))))
 
 (defn main []
-  (binding [main_resistors nil] (do (set! main_resistors [3.21389 2.0 3.0]) (println (str "Parallel: " (str (resistor_parallel main_resistors)))) (println (str "Series: " (str (resistor_series main_resistors)))))))
+  (binding [main_resistors nil] (do (set! main_resistors [3.21389 2.0 3.0]) (println (str "Parallel: " (mochi_str (resistor_parallel main_resistors)))) (println (str "Series: " (mochi_str (resistor_series main_resistors)))))))
 
 (defn -main []
   (let [rt (Runtime/getRuntime)
