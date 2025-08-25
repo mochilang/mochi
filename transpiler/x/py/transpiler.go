a232967f691233f70aee8181e0cb11dc7ac9ba07
Tam Nguyen Duc <1218621+tamnd@users.noreply.github.com>
2025-08-25 23:37:51 +0700

    py transpiler: enhance append handling

--- transpiler/x/py/transpiler.go
+++ transpiler/x/py/transpiler.go
@@ -132,6 +132,8 @@ const helperAppend = `
 def _append(lst, v):
     if lst is None:
         lst = []
+    elif not isinstance(lst, list):
+        lst = list(lst)
     lst.append(v)
     return lst
 `
